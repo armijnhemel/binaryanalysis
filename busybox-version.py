7f1a970f3edd5c003480a753b7e29dcc2f327271
Armijn Hemel <armijn@tjaldur.nl>
2010-04-14 20:56:51 +0000

    rework the busybox version tool

--- busybox-version.py
+++ busybox-version.py
@@ -8,22 +8,28 @@ import sys, os
 from optparse import OptionParser
 import busybox
 
-parser = OptionParser()
-parser.add_option("-b", "--binary", dest="bb", help="path to BusyBox binary", metavar="FILE")
-(options, args) = parser.parse_args()
-## suck in the BusyBox binary
-if options.bb == None:
-	parser.error("Path to BusyBox binary needed")
-try:
-	busybox_binary = open(options.bb, 'rb')
-except:
-	print "No valid BusyBox file"
-	sys.exit(1)
-busybox_lines = busybox_binary.read()
-## determine the BusyBox binary
-version = busybox.extract_version(busybox_lines)
+def busybox_version(path):
+	try:
+		busybox_binary = open(path, 'rb')
+		busybox_lines = busybox_binary.read()
+		return busybox.extract_version(busybox_lines)
+	except:
+		return None
+	
 
-if version != None:
-	print version
-else:
-	print "No BusyBox found"
+def main(argv):
+	parser = OptionParser()
+	parser.add_option("-b", "--binary", dest="bb", help="path to BusyBox binary", metavar="FILE")
+	(options, args) = parser.parse_args()
+	## suck in the BusyBox binary
+	if options.bb == None:
+		parser.error("Path to BusyBox binary needed")
+	version = busybox_version(options.bb)
+
+	if version != None:
+		print version
+	else:
+		print "No BusyBox found"
+
+if __name__ == "__main__":
+        main(sys.argv)
