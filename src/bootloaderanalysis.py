b4afaa817cedec0005e8d92b705e23242d3dc68d
Armijn Hemel <armijn@tjaldur.nl>
2010-04-21 10:51:34 +0000

    add scanning code for u-boot

--- src/bootloaderanalysis.py
+++ src/bootloaderanalysis.py
@@ -17,3 +17,25 @@ def searchRedBoot(path):
 
 def findRedBoot(lines):
 	return lines.find("Display RedBoot version information")
+
+def searchUBoot(path):
+        try:
+                binary = open(path, 'rb')
+                lines = binary.read()
+                if findUBoot(lines) != -1:
+                        return True
+                else:
+                        return None
+        except Exception, e:
+                return None
+
+def findUBoot(lines):
+        markerlines = [ "f_uboot_addr"
+                      , "f_firmware_addr"
+                      , "U-Boot 1.1.5-2.0"
+                      ]
+        for i in markerlines:
+                res = lines.find(i)
+                if res != -1:
+                        return res
+        return -1
