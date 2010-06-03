48497e313bf16c36f4832f37fc0386989c57ff3e
Armijn Hemel <armijn@tjaldur.nl>
2010-06-03 12:18:45 +0000

    add commandline options and add some data to the database

--- src/report2knowledgebase.py
+++ src/report2knowledgebase.py
@@ -21,8 +21,19 @@ from xml.xpath import Evaluate
 
 ## unpack the data and push them into the knowledgebase. This uses the configuration file to find the interesting file system sections
 ## This requires that the script generating the XML file uses the same configuration file as this file for consistency
-def process(xmldata, config):
-	print xmldata
+def process(xmldata, config, options):
+	## first put information regarding the device itself in the knowledgebase
+	c = conn.cursor()
+	t = (options.vendor, options.name, options.hwversion, options.chipset, options.upstream)
+	c.execute('''insert into device(vendor, name, version, chipset, upstream) values (?, ?, ?, ?, ?)''', t)
+	conn.commit()
+	lastrow = c.lastrowid
+	print lastrow
+
+	##
+	xmldoc = reader = PyExpat.Reader()
+	dom = reader.fromStream(xmldata)
+	print Evaluate('/file', dom)
 	## first put information regarding the top level firmware in the knowledgebase
 
 	## then search for all file systems, extract sha256, type, offset in the parent and put that in the knowledgebase
@@ -31,14 +42,20 @@ def main(argv):
         parser = OptionParser()
         parser.add_option("-c", "--config", action="store", dest="cfg", help="path to config file", metavar="FILE")
         parser.add_option("-d", "--database", action="store", dest="db", help="path to sqlite database", metavar="FILE")
+        parser.add_option("-n", "--name", action="store", dest="name", help="device name")
+        parser.add_option("-p", "--chipset", action="store", dest="chipset", help="chipset (optional)")
+        parser.add_option("-r", "--hwversion", action="store", dest="hwversion", help="device version/revision")
+        parser.add_option("-s", "--vendor", action="store", dest="vendor", help="device vendor")
+        parser.add_option("-u", "--upstream", action="store", dest="upstream", help="upstream vendor (optional)")
         parser.add_option("-x", "--xml", action="store", dest="xmlfile", help="path to XML file", metavar="FILE")
         (options, args) = parser.parse_args()
+
         if options.xmlfile == None:
                 parser.error("Path to XML file needed")
         try:
 		## quick sanity check to see if we have valid XML
         	xmldom = xml.dom.minidom.parse(options.xmlfile)
-        	xmldata = open(options.xmlfile).read()
+        	xmldata = open(options.xmlfile)
         except:
                 print "No valid XML file"
                 sys.exit(1)
@@ -70,7 +87,14 @@ def main(argv):
         ## use default system wide config
         else:
                 pass
-	process(xmldata, config)
+
+        if options.name == None:
+                parser.error("Device name needed")
+        if options.vendor == None:
+                parser.error("Vendor name needed")
+        if options.hwversion == None:
+                parser.error("Hardware version/revision needed")
+	process(xmldata, config, options)
 
 
 
