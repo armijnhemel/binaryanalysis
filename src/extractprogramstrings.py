a0cc378b059b5498e18b0600e9dbc1573b88bb57
Armijn Hemel <armijn@tjaldur.nl>
2010-09-13 23:37:28 +0000

    fix the regex. Some things do not get recognized correctly if a " is in a comment

--- src/extractprogramstrings.py
+++ src/extractprogramstrings.py
@@ -8,16 +8,48 @@ def extractsourcestrings(srcdir):
         try:
                 while True:
                         i = osgen.next()
+			#print i
                         for p in i[2]:
 				## we're only interested in a few files
 				if p.endswith('.c') or p.endswith('.h'):
-					print p
+					#print p
 					source = open("%s/%s" % (i[0], p)).read()
-					results = re.findall("(\".*\")", source, re.MULTILINE)
+					results = re.findall("\"(.*?)(?<!\\\)\"", source, re.MULTILINE|re.DOTALL)
 					for res in results:
-						print res
-	except:
-		pass
+                                        	## some strings are simply not interesting
+                                        	if res.strip() == "\\n":
+                                        		continue
+                                        	if res.strip() == "\\n\\n":
+                                                	continue
+                                        	elif res.strip() == "\\t":
+                                                	continue
+                                        	elif res.strip() == "%s%s":
+                                                	continue
+                                        	elif res.strip() == "%s:":
+                                                	continue
+                                        	elif res.strip() == "%s":
+                                                	continue
+                                        	elif res.strip() == "%d":
+                                                	continue
+                                        	elif res.strip() == ":":
+                                                	continue
+                                        	elif res.strip() == ",":
+                                                	continue
+                                        	elif res.strip() == "(":
+                                                	continue
+                                        	elif res.strip() == ")":
+                                                	continue
+                                        	elif res.strip() == "()":
+                                                	continue
+                                        	elif res.strip() == "|":
+                                                	continue
+                                        	elif res.strip() == "":
+                                                	continue
+						print res, p
+
+
+	except Exception, e:
+		print e
 
 
 def main(argv):
