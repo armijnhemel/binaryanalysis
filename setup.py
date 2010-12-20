a04f840f1fd3740ecb3dac43229616f8a38dcd67
Armijn Hemel <armijn@tjaldur.nl>
2010-12-20 19:06:25 +0000

    stubs for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Binary Analysis Tool',
+      version='1.0',
+      description='Binary Analysis Tool',
+      author='Loohuis Consulting',
+      author_email='info@binaryanalysis.org',
+      url='http://www.binaryanalysis.org/',
+      packages=[''],
+     )
