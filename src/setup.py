65dd0b3163bd59a30c16f47f458fd9c99c68257f
Armijn Hemel <armijn@tjaldur.nl>
2010-12-21 23:23:31 +0000

    more packaging stubs

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import glob
+import os.path
 
-setup(name='Binary Analysis Tool',
+setup(name='bat',
       version='1.0',
       description='Binary Analysis Tool',
       author='Loohuis Consulting',
       author_email='info@binaryanalysis.org',
       url='http://www.binaryanalysis.org/',
       packages=[''],
+      scripts=['bruteforce.py', 'busybox-compare-configs.py'],
+      data_files=[('/etc/bat/configs', glob.glob('configs/*')),
+                  ('/etc/bat',  ['bruteforce-config']),
+                  ('share/doc/bat-1.0',  glob.glob('../doc/*')),
+                 ]
      )
