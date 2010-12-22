2b9da6dfd49faf50d9d7f58dbc4151e850beee7a
Armijn Hemel <armijn@tjaldur.nl>
2010-12-22 16:24:03 +0000

    more packaging

--- src/setup.py
+++ src/setup.py
@@ -11,9 +11,17 @@ setup(name='bat',
       author_email='info@binaryanalysis.org',
       url='http://www.binaryanalysis.org/',
       packages=[''],
+      license="Apache 2.0",
       scripts=['bruteforce.py', 'busybox-compare-configs.py'],
       data_files=[('/etc/bat/configs', glob.glob('configs/*')),
                   ('/etc/bat',  ['bruteforce-config']),
                   ('share/doc/bat-1.0',  glob.glob('../doc/*')),
-                 ]
+                 ],
+     long_description="""The Binary Analysis Tool is a modular framework that assists with auditing
+the contents of compiled software. It makes it easier and cheaper to look
+inside technology, and this helps compliance and due diligence activities.
+
+The tool is freely available to everyone. The community can use it and
+participate in further development, and work together to help reduce errors
+when shipping devices or products containing Free and Open Source Software."""
      )
