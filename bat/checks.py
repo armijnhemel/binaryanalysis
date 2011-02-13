4bccb77c3cd0e22e4667c742adf41af9467b078d
Armijn Hemel <armijn@tjaldur.nl>
2011-02-13 18:53:03 +0000

    inline checks, move some checks into one file

--- bat/checks.py
+++ bat/checks.py
@@ -8,26 +8,20 @@ import string, re
 import extractor
 
 def searchLoadLin(path, blacklist=[]):
-        try:
-                binary = open(path, 'rb')
-                lines = binary.read()
-                offset = extractLoadLin(lines)
-		if offset != -1 and not extractor.inblacklist(offset, blacklist):
-			return True
-		else:
-			return None
-        except Exception, e:
-                return None
-
-def extractLoadLin(lines):
 	markerStrings = [ 'Ooops..., size of "setup.S" has become too long for LOADLIN,'
 			, 'LOADLIN started from $'
 			]
-	for marker in markerStrings:
-		res = lines.find(marker)
-		if res != -1:
-			return res
-	return -1
+        try:
+                binary = open(path, 'rb')
+                lines = binary.read()
+		for marker in markerStrings:
+			offset = lines.find(marker)
+			if offset != -1 and not extractor.inblacklist(offset, blacklist):
+				return True
+			else:
+				return None
+        except Exception, e:
+                return None
 
 def searchIptables(path, blacklist=[]):
 	markerStrings = [ 'iptables who? (do you need to insmod?)'
@@ -46,129 +40,88 @@ def searchIptables(path, blacklist=[]):
         except Exception, e:
                 return None
 
-def extractIptables(lines):
-	markerStrings = [ 'iptables who? (do you need to insmod?)'
-			, 'Will be implemented real soon.  I promise ;)'
-			, 'can\'t initialize iptables table `%s\': %s'
-			]
-	for marker in markerStrings:
-		res = lines.find(marker)
-		if res != -1:
-			return res
-	return -1
-
 def searchDproxy(path, blacklist=[]):
-        try:
-                binary = open(path, 'rb')
-                lines = binary.read()
-                offset = extractDproxy(lines)
-		if offset != -1 and not extractor.inblacklist(offset, blacklist):
-			return True
-		else:
-			return None
-        except Exception, e:
-                return None
-
-def extractDproxy(lines):
 	markerStrings = [ '# dproxy monitors this file to determine when the machine is'
 			, '# If you want dproxy to log debug info specify a file here.'
 			]
-	for marker in markerStrings:
-		res = lines.find(marker)
-		if res != -1:
-			return res
-	return -1
-
-def searchEzIpupdate(path, blacklist=[]):
         try:
                 binary = open(path, 'rb')
                 lines = binary.read()
-                offset = extractEzIpupdate(lines)
-		if offset != -1 and not extractor.inblacklist(offset, blacklist):
-			return True
-		else:
-			return None
+		for marker in markerStrings:
+			offset = lines.find(marker)
+			if offset != -1 and not extractor.inblacklist(offset, blacklist):
+				return True
+			else:
+				return None
         except Exception, e:
                 return None
 
-def extractEzIpupdate(lines):
+def searchEzIpupdate(path, blacklist=[]):
 	markerStrings = [ 'ez-ipupdate Version %s, Copyright (C) 1998-'
 			, '%s says that your IP address has not changed since the last update'
 			, 'you must provide either an interface or an address'
 			]
-	for marker in markerStrings:
-		res = lines.find(marker)
-		if res != -1:
-			return res
-	return -1
-
-def searchLibusb(path, blacklist=[]):
         try:
                 binary = open(path, 'rb')
                 lines = binary.read()
-                offset = extractLibusb(lines)
-		if offset != -1 and not extractor.inblacklist(offset, blacklist):
-			return True
-		else:
-			return None
+		for marker in markerStrings:
+			offset = lines.find(marker)
+			if offset != -1 and not extractor.inblacklist(offset, blacklist):
+				return True
+			else:
+				return None
         except Exception, e:
                 return None
 
-def extractLibusb(lines):
+def searchLibusb(path, blacklist=[]):
 	markerStrings = [ 'Check that you have permissions to write to %s/%s and, if you don\'t, that you set up hotplug (http://linux-hotplug.sourceforge.net/) correctly.'
 			, 'usb_os_find_busses: Skipping non bus directory %s'
 			, 'usb_os_init: couldn\'t find USB VFS in USB_DEVFS_PATH'
 			]
-	for marker in markerStrings:
-		res = lines.find(marker)
-		if res != -1:
-			return res
-	return -1
-
-def searchVsftpd(path, blacklist=[]):
         try:
                 binary = open(path, 'rb')
                 lines = binary.read()
-                offset = extractVsftpd(lines)
-		if offset != -1 and not extractor.inblacklist(offset, blacklist):
-			return True
-		else:
-			return None
+		for marker in markerStrings:
+			offset = lines.find(marker)
+			if offset != -1 and not extractor.inblacklist(offset, blacklist):
+				return True
+			else:
+				return None
         except Exception, e:
                 return None
 
-def extractVsftpd(lines):
+def searchVsftpd(path, blacklist=[]):
 	markerStrings = [ 'vsftpd: version'
 			, '(vsFTPd '
 			, 'VSFTPD_LOAD_CONF'
 			, 'run two copies of vsftpd for IPv4 and IPv6'
 			]
-	for marker in markerStrings:
-		res = lines.find(marker)
-		if res != -1:
-			return res
-	return -1
-
-def searchHostapd(path, blacklist=[]):
         try:
                 binary = open(path, 'rb')
                 lines = binary.read()
-                offset = extractHostapd(lines)
-		if offset != -1 and not extractor.inblacklist(offset, blacklist):
-			return True
-		else:
-			return None
+		for marker in markerStrings:
+			offset = lines.find(marker)
+			if offset != -1 and not extractor.inblacklist(offset, blacklist):
+				return True
+			else:
+				return None
         except Exception, e:
                 return None
 
-def extractHostapd(lines):
+def searchHostapd(path, blacklist=[]):
 	markerStrings = [ 'hostapd v'
 			]
-	for marker in markerStrings:
-		res = lines.find(marker)
-		if res != -1:
-			return res
-	return -1
+        try:
+                binary = open(path, 'rb')
+                lines = binary.read()
+		for marker in markerStrings:
+			offset = lines.find(marker)
+			if offset != -1 and not extractor.inblacklist(offset, blacklist):
+				return True
+			else:
+				return None
+        except Exception, e:
+                return None
 
 def searchWpaSupplicant(path, blacklist=[]):
 	markerStrings = [ 'wpa_supplicant v'
@@ -185,34 +138,38 @@ def searchWpaSupplicant(path, blacklist=[]):
 	except Exception, e:
 		return None
 
-def extractWpaSupplicant(lines):
-	markerStrings = [ 'wpa_supplicant v'
-			]
-	for marker in markerStrings:
-		res = lines.find(marker)
-		if res != -1:
-			return res
-	return -1
-
 def searchIproute(path, blacklist=[]):
-        try:
-                binary = open(path, 'rb')
-                lines = binary.read()
-                offset = extractIproute(lines)
-		if offset != -1 and not extractor.inblacklist(offset, blacklist):
-			return True
-		else:
-			return None
-        except Exception, e:
-                return None
-
-def extractIproute(lines):
 	markerStrings = [ 'Usage: tc [ OPTIONS ] OBJECT { COMMAND | help }'
 			, 'tc utility, iproute2-ss%s'
 			, 'Option "%s" is unknown, try "tc -help".'
 			]
-	for marker in markerStrings:
-		res = lines.find(marker)
-		if res != -1:
-			return res
-	return -1
+        try:
+                binary = open(path, 'rb')
+                lines = binary.read()
+		for marker in markerStrings:
+			offset = lines.find(marker)
+			if offset != -1 and not extractor.inblacklist(offset, blacklist):
+				return True
+			else:
+				return None
+        except Exception, e:
+                return None
+
+def searchWirelessTools(path, blacklist=[]):
+	wirelessMarkerStrings = [ "Driver has no Wireless Extension version information."
+				, "Wireless Extension version too old."
+				, "Wireless-Tools version"
+				, "Wireless Extension, while we are using version %d."
+				, "Currently compiled with Wireless Extension v%d."
+       	                 ]
+        try:
+                wireless_binary = open(path, 'rb')
+                lines = wireless_binary.read()
+		for marker in wirelessMarkerStrings:
+			offset = lines.find(marker)
+			if offset != -1 and not extractor.inblacklist(offset, blacklist):
+				return True
+			else:
+				return None
+        except Exception, e:
+                return None
