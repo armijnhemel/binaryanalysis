2d29c9186e72ecf311bb60b21e553971af01bdb7
Armijn Hemel <armijn@tjaldur.nl>
2011-07-31 13:27:25 +0000

    move RPM unpacking

--- bat/fwunpack.py
+++ bat/fwunpack.py
@@ -13,16 +13,12 @@ file system in the parent file.
 
 Optionally, we return a range of bytes that should be excluded in same cases
 where we want to prevent other scans from (re)scanning (part of) the data.
-
-TODO: merge many of the searchUnpack and unpack methods, so we only have to
-suck in the data once in the unpack part.
 '''
 
 import sys, os, subprocess, os.path
 import tempfile, bz2, re, magic, tarfile
 import fsmagic, fssearch, extractor, ext2
 from xml.dom import minidom
-import rpm
 
 ## generic method to create temporary directories, with the correct filenames
 ## which is used throughout the code.
@@ -1289,73 +1285,6 @@ def unpackLZMA(data, offset, tempdir=None):
 	os.fdopen(outtmpfile[0]).close()
 	os.unlink(tmpfile[1])
 	return tmpdir
-
-def unpackRPM(data, offset, tempdir=None):
-	## Assumes (for now) that rpm2cpio is in the path
-	tmpdir = unpacksetup(tempdir)
-	tmpfile = tempfile.mkstemp(dir=tmpdir)
-	os.write(tmpfile[0], data[offset:])
-	## first use rpm2cpio to unpack the rpm data
-	p = subprocess.Popen(['rpm2cpio', tmpfile[1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-	(stanout, stanerr) = p.communicate()
-	if len(stanout) != 0:
-		## cleanup first
-                os.fdopen(tmpfile[0]).close()
-                os.unlink(tmpfile[1])
-		if tempdir == None:
-                	os.rmdir(tmpdir)
-		## then use unpackCpio() to unpack the RPM
-		return unpackCpio(stanout, 0, tempdir)
-	else:
-                os.fdopen(tmpfile[0]).close()
-                os.unlink(tmpfile[1])
-		if tempdir == None:
-                	os.rmdir(tmpdir)
-		return None
-
-## RPM is basically a header, plus some compressed files, so we are getting
-## duplicates at the moment. We can defeat this easily by setting the blacklist
-## upperbound to the start of compression.
-def searchUnpackRPM(filename, tempdir=None, blacklist=[], offsets={}):
-	datafile = open(filename, 'rb')
-	offset = fssearch.findRPM(datafile)
-	if offset == -1:
-		datafile.close()
-		return ([], blacklist, offsets)
-	else:
-		diroffsets = []
-		rpmcounter = 1
-		data = datafile.read()
-		while(offset != -1):
-			blacklistoffset = extractor.inblacklist(offset, blacklist)
-			if blacklistoffset != None:
-				offset = fssearch.findRPM(datafile, blacklistoffset)
-			if offset == -1:
-				break
-			tmpdir = dirsetup(tempdir, filename, "rpm", rpmcounter)
-			res = unpackRPM(data, offset, tmpdir)
-			if res != None:
-				diroffsets.append((res, offset))
-				rpmcounter = rpmcounter + 1
-				## determine which compression is used, so we can
-				## find the right offset. Code from the RPM examples
-				tset = rpm.TransactionSet()
-        			fdno = os.open(filename, os.O_RDONLY)
-        			header = tset.hdrFromFdno(fdno)
-        			os.close(fdno)
-				## first some sanity checks. payload format should
-				## always be 'cpio' according to LSB 3
-				if header[rpm.RPMTAG_PAYLOADFORMAT] == 'cpio':
-					## compression should always be 'gzip' according to LSB 3
-					if header[rpm.RPMTAG_PAYLOADCOMPRESSOR] == 'gzip':
-						payloadoffset = fssearch.findGzip(datafile, offset)
-						blacklist.append((offset, payloadoffset))
-			else:
-				## cleanup
-				os.rmdir(tmpdir)
-			offset = fssearch.findRPM(datafile, offset+1)
-		datafile.close()
-		return (diroffsets, blacklist, offsets)
 
 ## Search and unpack Ubifs. Since we can't easily determine the length of the
 ## file system by using ubifs we will have to use a different measurement to
---
+++ bat/unpackrpm.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+## Binary Analysis Tool
+## Copyright 2009-2011 Armijn Hemel for Tjaldur Software Governance Solutions
+## Licensed under Apache 2.0, see LICENSE file for details
+
+'''
+This module contains only the RPM specific parts. This is so we don't get
+failures in the systems that don't have Python RPM bindings.
+'''
+
+import sys, os, subprocess, os.path
+import tempfile, magic, rpm
+import fsmagic, fssearch, extractor, fwunpack
+
+def unpackRPM(data, offset, tempdir=None):
+	## Assumes (for now) that rpm2cpio is in the path
+	tmpdir = fwunpack.unpacksetup(tempdir)
+	tmpfile = tempfile.mkstemp(dir=tmpdir)
+	os.write(tmpfile[0], data[offset:])
+	## first use rpm2cpio to unpack the rpm data
+	p = subprocess.Popen(['rpm2cpio', tmpfile[1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+	(stanout, stanerr) = p.communicate()
+	if len(stanout) != 0:
+		## cleanup first
+                os.fdopen(tmpfile[0]).close()
+                os.unlink(tmpfile[1])
+		if tempdir == None:
+                	os.rmdir(tmpdir)
+		## then use unpackCpio() to unpack the RPM
+		return fwunpack.unpackCpio(stanout, 0, tempdir)
+	else:
+                os.fdopen(tmpfile[0]).close()
+                os.unlink(tmpfile[1])
+		if tempdir == None:
+                	os.rmdir(tmpdir)
+		return None
+
+## RPM is basically a header, plus some compressed files, so we are getting
+## duplicates at the moment. We can defeat this easily by setting the blacklist
+## upperbound to the start of compression.
+def searchUnpackRPM(filename, tempdir=None, blacklist=[], offsets={}):
+	datafile = open(filename, 'rb')
+	offset = fssearch.findRPM(datafile)
+	if offset == -1:
+		datafile.close()
+		return ([], blacklist, offsets)
+	else:
+		diroffsets = []
+		rpmcounter = 1
+		data = datafile.read()
+		while(offset != -1):
+			blacklistoffset = extractor.inblacklist(offset, blacklist)
+			if blacklistoffset != None:
+				offset = fssearch.findRPM(datafile, blacklistoffset)
+			if offset == -1:
+				break
+			tmpdir = fwunpack.dirsetup(tempdir, filename, "rpm", rpmcounter)
+			res = unpackRPM(data, offset, tmpdir)
+			if res != None:
+				diroffsets.append((res, offset))
+				rpmcounter = rpmcounter + 1
+				## determine which compression is used, so we can
+				## find the right offset. Code from the RPM examples
+				tset = rpm.TransactionSet()
+        			fdno = os.open(filename, os.O_RDONLY)
+        			header = tset.hdrFromFdno(fdno)
+        			os.close(fdno)
+				## first some sanity checks. payload format should
+				## always be 'cpio' according to LSB 3
+				if header[rpm.RPMTAG_PAYLOADFORMAT] == 'cpio':
+					## compression should always be 'gzip' according to LSB 3
+					if header[rpm.RPMTAG_PAYLOADCOMPRESSOR] == 'gzip':
+						payloadoffset = fssearch.findGzip(datafile, offset)
+						blacklist.append((offset, payloadoffset))
+			else:
+				## cleanup
+				os.rmdir(tmpdir)
+			offset = fssearch.findRPM(datafile, offset+1)
+		datafile.close()
+		return (diroffsets, blacklist, offsets)
