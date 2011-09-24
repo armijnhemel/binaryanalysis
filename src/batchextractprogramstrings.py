335cdd0731ced328d0faaf99ef3db9367ed620ee
Armijn Hemel <armijn@tjaldur.nl>
2011-09-24 12:51:48 +0000

    always run fossology and don't let it rely on ninkacomments, since license texts could appear halfway in a file

--- src/batchextractprogramstrings.py
+++ src/batchextractprogramstrings.py
@@ -194,6 +194,7 @@ def extractstrings(srcdir, conn, cursor, package, version, license):
 								## store all the licenses we already know for this file
 								for r in res:
 									(filelicense, scannerversion) = r
+									print >>sys.stderr, "NINKA     %s/%s" % (i[0],p), filelicense
 									## hardcode the scanner to 'ninka'. This could/should change in the future.
 									cursor.execute('''insert into licenses (sha256, license, scanner, version) values (?,?,?,?)''', (filehash, filelicense, "ninka", scannerversion))
 							else:
@@ -214,19 +215,19 @@ def extractstrings(srcdir, conn, cursor, package, version, license):
 										print >>sys.stderr, "NINKA     %s/%s" % (i[0],p), license
 										cursor.execute('''insert into licenses (sha256, license, scanner, version) values (?,?,?,?)''', (filehash, license, "ninka", "594d5e4"))
 										cursor.execute('''insert into ninkacomments (sha256, license, scanner, version) values (?,?,?,?)''', (commentshash, license, "ninka", "594d5e4"))
-								## Also run FOSSology
-								## this requires that the user has enough privileges to actually connect to the FOSSology database!
-								p2 = subprocess.Popen(["/usr/lib/fossology/agents/nomos", "%s/%s" % (i[0], p)], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-                                				(stanout, stanerr) = p2.communicate()
-								if "FATAL" in stanout:
-									pass
-								else:
-									fossysplit = stanout.strip().rsplit(" ", 1)
-									licenses = fossysplit[-1].split(',')
-									for license in licenses:
-										print >>sys.stderr, "FOSSOLOGY %s/%s" % (i[0],p), license
-										cursor.execute('''insert into licenses (sha256, license, scanner, version) values (?,?,?,?)''', (filehash, license, "nomos", "1.4.0"))
-								print >> sys.stderr
+							## Also run FOSSology. Since licenses might appear halfway a file we should not look at the ninkacomments table!
+							## This requires that the user has enough privileges to actually connect to the FOSSology database!
+							p2 = subprocess.Popen(["/usr/lib/fossology/agents/nomos", "%s/%s" % (i[0], p)], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                                			(stanout, stanerr) = p2.communicate()
+							if "FATAL" in stanout:
+								pass
+							else:
+								fossysplit = stanout.strip().rsplit(" ", 1)
+								licenses = fossysplit[-1].split(',')
+								for license in licenses:
+									print >>sys.stderr, "FOSSOLOGY %s/%s" % (i[0],p), license
+									cursor.execute('''insert into licenses (sha256, license, scanner, version) values (?,?,?,?)''', (filehash, license, "nomos", "1.4.0"))
+							print >> sys.stderr
 						sqlres = extractsourcestrings(p, i[0], package, version, srcdirlen)
 						for res in sqlres:
 							(pstring, linenumber) = res
