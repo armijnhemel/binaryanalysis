431d7b9164088f7678050d61fbeea4fea6814ceb
Armijn Hemel <armijn@tjaldur.nl>
2011-12-16 19:49:31 +0000

    classify some licenses differently

--- bat/checks.py
+++ bat/checks.py
@@ -246,9 +246,10 @@ def pdfPrettyPrint(res, root):
 ## never as proof that a binary is actually licensed under a license!
 def scanLicenses(path, blacklist=[], envvars=None):
 	results = {}
-	if genericSearch(path, ["General Public License", "http://gnu.org/licenses/gpl.html",
-                                "http://www.gnu.org/licenses/gpl.html", "http://www.opensource.org/licenses/gpl-license.php",
-                                "http://www.gnu.org/copyleft/gpl.html"]):
+	if genericSearch(path, ["General Public License", "http://www.gnu.org/licenses/"]:
+		results['GNU'] = True
+	if genericSearch(path, ["http://gnu.org/licenses/gpl.html", "http://www.gnu.org/licenses/gpl.html",
+                                "http://www.opensource.org/licenses/gpl-license.php", "http://www.gnu.org/copyleft/gpl.html"]):
 		results['GPL'] = True
 	if genericSearch(path, ["http://gnu.org/licenses/gpl-2.0.html", "http://www.gnu.org/licenses/old-licenses/gpl-2.0.html"]):
 		results['GPLv2'] = True
