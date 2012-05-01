b06a159414aeed3863238464438c5cce4e15d47a
Armijn Hemel <armijn@tjaldur.nl>
2012-05-01 00:31:19 +0000

    start on version using pylab

--- src/bat/images.py
+++ src/bat/images.py
@@ -139,6 +139,34 @@ def generateImages(filename, unpackreport, leafscans, scantempdir, toplevelscand
 				pylab.gcf().clear()
 				for j in i['ranking']['reports']:
 					if j[4] != {}:
+						'''
+						j_sorted = sorted(j[4], key=lambda x: j[4][x])
+						max_y = j[4][j_sorted[-1]]
+						xvalues = []
+						yvalues = []
+						for v in j_sorted:
+							xvalues.append(v)
+							yvalues.append(j[4][v])
+							print >>sys.stderr, v, j[4][v], j[1], xvalues, yvalues, "max", max_y
+
+						figsize = len(xvalues) * 1.0
+						pylab.gcf().set_size_inches(figsize, 7)
+
+						pylab.xlabel('version')
+						pylab.ylabel('matches')
+						pylab.title("Unique matches for %s" % j[1])
+						## leave some space at the top
+						pylab.gca().set_ylim(top=max_y + 1)
+						x = pylab.arange(len(xvalues))
+						b = pylab.bar(x, yvalues, width=0.6)
+						for bb in b:
+							print >>sys.stderr, bb.get_width(), bb.get_height()
+						## center the text
+						pylab.xticks(x+0.3, xvalues, rotation=270)
+
+						pylab.savefig('%s/%s-%s-version.png' % (imagedir, unpackreport['sha256'], j[1]))
+						pylab.gcf().clear()
+						'''
 						tmppickle = tempfile.mkstemp()
 						pickledata = []
 						j_sorted = sorted(j[4], key=lambda x: j[4][x])
