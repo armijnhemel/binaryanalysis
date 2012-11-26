a6a6e6d64871aa1d5d70cd0dc0362ee2bd74e7e9
Armijn Hemel <armijn@tjaldur.nl>
2012-11-26 13:38:45 +0000

    replace hardcoded (non-functional) tags check by options in configuration file

--- src/bat/images.py
+++ src/bat/images.py
@@ -31,15 +31,6 @@ from PIL import Image
 def generateImages(filename, unpackreport, leafscans, scantempdir, toplevelscandir, envvars={}):
 	if not unpackreport.has_key('sha256'):
 		return
-	ignorelist = ['graphics', 'text', 'compressed', 'pdf', 'xml', 'resources']
-	## not interested in text files, graphics or compressed files
-	## TODO: make this configurable
-	for s in leafscans:
-		if s.keys()[0] == 'tags':
-			for i in ignorelist:
-				if i in s['tags']:
-					return
-
 	scanenv = os.environ.copy()
 	if envvars != None:
 		for en in envvars.split(':'):
