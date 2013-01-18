67e45834b908c146c472af849289830269ce1a3c
Armijn Hemel <armijn@tjaldur.nl>
2013-01-18 22:00:07 +0000

    spit out results for function names

--- src/bat/generatenameshtml.py
+++ src/bat/generatenameshtml.py
@@ -37,39 +37,114 @@ def generateHTML(filename, unpackreport, leafscans, scantempdir, toplevelscandir
 			return
 
 	if leafscans.has_key('ranking') :
-		header = "<html><body>"
-		html = ""
 		## the ranking result is (res, dynamicRes, variablepvs)
 		(res, dynamicRes, variablepvs) = leafscans['ranking']
-		if variablepvs == {}:
+		if variablepvs == {} and dynamicRes == {}:
 			return
-		language = variablepvs['language']
-		## we should move function name reporting to here
-		#if dynamicRes != {}:
-		#	pass
 
-		if language == 'Java':
-			fieldspackages = {}
-			sourcespackages = {}
-			classespackages = {}
-			fieldscount = {}
-			sourcescount = {}
-			classescount = {}
-			for i in ['classes', 'sources', 'fields']:
-				if not variablepvs.has_key(i):
-					continue
-				packages = {}
-				packagecount = {}
-				if variablepvs[i] != []:
-					for c in variablepvs[i]:
-						lenres = len(list(set(map(lambda x: x[0], variablepvs[i][c]))))
+		if dynamicRes != {}:
+			header = "<html><body>"
+			html = ""
+			if dynamicRes.has_key('uniquepackages'):
+				if dynamicRes['uniquepackages'] != {}:
+					for i in dynamicRes['uniquepackages'].keys():
+						html += "<h3>Unique function name matches for %s (%d)</h3><p>\n" % (i, len(dynamicRes['uniquepackages'][i]))
+						for v in dynamicRes['uniquepackages'][i]:
+							html += "%s</br>\n" % v
+						html += "</p>\n"
+			footer = "</body></html>"
+			if html != "":
+				html = header + html + footer
+				nameshtmlfile = gzip.open("%s/%s-functionnames.html.gz" % (reportdir, unpackreport['sha256']), 'wb')
+				nameshtmlfile.write(html)
+				nameshtmlfile.close()
+		if variablepvs != {}:
+			header = "<html><body>"
+			html = ""
+			language = variablepvs['language']
+
+			if language == 'Java':
+				fieldspackages = {}
+				sourcespackages = {}
+				classespackages = {}
+				fieldscount = {}
+				sourcescount = {}
+				classescount = {}
+				for i in ['classes', 'sources', 'fields']:
+					if not variablepvs.has_key(i):
+						continue
+					packages = {}
+					packagecount = {}
+					if variablepvs[i] != []:
+						for c in variablepvs[i]:
+							lenres = len(list(set(map(lambda x: x[0], variablepvs[i][c]))))
+							if lenres == 1:
+								pvs = variablepvs[i][c]
+								(package,version) = variablepvs[i][c][0]
+								if packagecount.has_key(package):
+									packagecount[package] = packagecount[package] + 1
+								else:
+									packagecount[package] = 1
+								'''
+								## for later use
+								for p in pvs:
+									(package,version) = p
+									if packages.has_key(package):
+										packages[package].append(version)
+									else:
+										packages[package] = [version]
+								'''
+					if packagecount != {}:
+						if i == 'classes':
+							classescount = packagecount
+						if i == 'sources':
+							sourcescount = packagecount
+						if i == 'fields':
+							fieldscount = packagecount
+	
+					if packages != {}:
+						if i == 'classes':
+							classespackages = packages
+						if i == 'sources':
+							sourcespackages = packages
+						if i == 'fields':
+							fieldspackages = packages
+	
+				if classescount != {}:
+					html = html + "<h3>Unique matches of class names</h3>\n<table>\n"
+					html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+					for i in classescount:
+						html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, classescount[i])
+					html = html + "</table>\n"
+	
+				if sourcescount != {}:
+					html = html + "<h3>Unique matches of source file names</h3>\n<table>\n"
+					html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+					for i in sourcescount:
+						html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, sourcescount[i])
+					html = html + "</table>\n"
+	
+				if fieldscount != {}:
+					html = html + "<h3>Unique matches of field names</h3>\n<table>\n"
+					html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+					for i in fieldscount:
+						html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, fieldscount[i])
+					html = html + "</table>\n"
+	
+			if language == 'C':
+				if variablepvs.has_key('variables'):
+					packages = {}
+					packagecount = {}
+					for c in variablepvs['variables']:
+						lenres = len(list(set(map(lambda x: x[0], variablepvs['variables'][c]))))
 						if lenres == 1:
-							pvs = variablepvs[i][c]
-							(package,version) = variablepvs[i][c][0]
+							pvs = variablepvs['variables'][c]
+							(package,version) = variablepvs['variables'][c][0]
 							if packagecount.has_key(package):
 								packagecount[package] = packagecount[package] + 1
 							else:
 								packagecount[package] = 1
+								
 							'''
 							## for later use
 							for p in pvs:
@@ -79,77 +154,17 @@ def generateHTML(filename, unpackreport, leafscans, scantempdir, toplevelscandir
 								else:
 									packages[package] = [version]
 							'''
-				if packagecount != {}:
-					if i == 'classes':
-						classescount = packagecount
-					if i == 'sources':
-						sourcescount = packagecount
-					if i == 'fields':
-						fieldscount = packagecount
-
-				if packages != {}:
-					if i == 'classes':
-						classespackages = packages
-					if i == 'sources':
-						sourcespackages = packages
-					if i == 'fields':
-						fieldspackages = packages
-
-			if classescount != {}:
-				html = html + "<h3>Unique matches of class names</h3>\n<table>\n"
-				html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
-				for i in classescount:
-					html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, classescount[i])
-				html = html + "</table>\n"
-
-			if sourcescount != {}:
-				html = html + "<h3>Unique matches of source file names</h3>\n<table>\n"
-				html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
-				for i in sourcescount:
-					html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, sourcescount[i])
-				html = html + "</table>\n"
-
-			if fieldscount != {}:
-				html = html + "<h3>Unique matches of field names</h3>\n<table>\n"
-				html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
-				for i in fieldscount:
-					html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, fieldscount[i])
-				html = html + "</table>\n"
-
-		if language == 'C':
-			if variablepvs.has_key('variables'):
-				packages = {}
-				packagecount = {}
-				for c in variablepvs['variables']:
-					lenres = len(list(set(map(lambda x: x[0], variablepvs['variables'][c]))))
-					if lenres == 1:
-						pvs = variablepvs['variables'][c]
-						(package,version) = variablepvs['variables'][c][0]
-						if packagecount.has_key(package):
-							packagecount[package] = packagecount[package] + 1
-						else:
-							packagecount[package] = 1
-							
-						'''
-						## for later use
-						for p in pvs:
-							(package,version) = p
-							if packages.has_key(package):
-								packages[package].append(version)
-							else:
-								packages[package] = [version]
-						'''
-
-				if packagecount != {}:
-					html = html + "<h3>Unique matches of variables</h3>\n<table>\n"
-					html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
-					for i in packagecount:
-						html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, packagecount[i])
-					html = html + "</table>\n"
-
-		footer = "</body></html>"
-		if html != "":
-			html = header + html + footer
-			nameshtmlfile = gzip.open("%s/%s-names.html.gz" % (reportdir, unpackreport['sha256']), 'wb')
-			nameshtmlfile.write(html)
-			nameshtmlfile.close()
+	
+					if packagecount != {}:
+						html = html + "<h3>Unique matches of variables</h3>\n<table>\n"
+						html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+						for i in packagecount:
+							html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, packagecount[i])
+						html = html + "</table>\n"
+	
+			footer = "</body></html>"
+			if html != "":
+				html = header + html + footer
+				nameshtmlfile = gzip.open("%s/%s-names.html.gz" % (reportdir, unpackreport['sha256']), 'wb')
+				nameshtmlfile.write(html)
+				nameshtmlfile.close()
