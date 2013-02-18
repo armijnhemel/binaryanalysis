0ee9a6611fe2aa2cb6b11bc864329736867c3e60
Armijn Hemel <armijn@tjaldur.nl>
2013-02-18 13:31:59 +0000

    merge generatenameshtml.py and generateuniquehtml.py

--- src/bat/generateuniquehtml.py
+++ src/bat/generateuniquehtml.py
@@ -83,85 +83,225 @@ def generateHTML(filename, unpackreport, scantempdir, topleveldir, envvars={}):
 	if leafreports.has_key('ranking') :
 		## the ranking result is (res, dynamicRes, variablepvs)
 		(res, dynamicRes, variablepvs) = leafreports['ranking']
-		if res == None:
-			return
-		if res['reports'] != []:
-			htmllinks = []
-			for j in res['reports']:
-				if len(j[2]) != 0:
-					## here we should either do a database lookup to get the checksum,
-					## or check if they are already in the report
-					htmllinks.append((j[1], j[2]))
-			if htmllinks != []:
-				uniquehtml = "<html><body><h1>Unique matches per package</h1><p><ul>"
-				## first generate a header
-				for h in htmllinks:
-					uniquehtml = uniquehtml + "<li><a href=\"#%s\">%s (%d)</a>" % (h[0], h[0], len(h[1]))
-				uniquehtml = uniquehtml + "</ul></p>"
-				for h in htmllinks:
-					uniquehtml = uniquehtml + "<hr><h2><a name=\"%s\" href=\"#%s\">Matches for: %s (%d)</a></h2>" % (h[0], h[0], h[0], len(h[1]))
-					for k in h[1]:
-						## we have a list of tuples, per unique string we have a list of sha256sums and meta info
-						## This is really hairy
-						if len(k) > 1:
-							uniquehtml = uniquehtml + "<h5>%s</h5><p><table><tr><td><b>Filename</b></td><td><b>Version(s)</b></td><td><b>Line number</b></td><td><b>SHA256</b></td></tr>" % cgi.escape(k[0])
-							uniqtablerows = []
-							sh = {}
-							for s in k[1]:
-								## if possible, remove the package name, plus version number, from the path
-								## that is displayed. This is to prevent that a line is printed for every
-								## version, even when the code has not changed. Usually it will be clear
-								## which file is meant.
-								(pv, fp) = s[3].split('/', 1)
-								## clean up some names first, especially when they have been changed by Debian
-								for e in ["+dfsg", "~dfsg", ".orig", ".dfsg1", ".dfsg2"]:
-									if pv.endswith(e):
-										pv = pv[:-len(e)]
-										break
-								if pv == "%s-%s" % (h[0], s[1]) or pv == "%s_%s" % (h[0], s[1]):
-									if sh.has_key(s[0]):
-										sh[s[0]].append((fp,s[1], s[2]))
-									else:
-										sh[s[0]] = [(fp, s[1], s[2])]
-								else:	
-									if sh.has_key(s[0]):
-										sh[s[0]].append((s[3],s[1], s[2]))
-									else:
-										sh[s[0]] = [(s[3], s[1], s[2])]
-							for s in sh:
-								## per checksum we have a list of (filename, version)
-								## Now we need to check if we only have one filename, or if there are multiple.
-								## If there is just one it is easy:
-								if len(set(map(lambda x: x[0], sh[s]))) == 1:
-									lines = sorted(set(map(lambda x: (x[2]), sh[s])))
-									versions = sorted(set(map(lambda x: (x[1]), sh[s])))
-									versionline = squash_versions(versions)
-									numlines = reduce(lambda x, y: x + ", " + y, map(lambda x: "<a href=\"unique:/%s#%d\">%d</a>" % (s, x, x), lines))
-									uniqtablerows.append("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n" % (sh[s][0][0], versionline, numlines, s))
-								else:
-									for d in list(set(map(lambda x: x[0], sh[s]))):
-										filterd = filter(lambda x: x[0] == d, sh[s])
-										lines = sorted(set(map(lambda x: (x[2]), filterd)))
-
-										versions = sorted(set(map(lambda x: (x[1]), filterd)))
+		if res != None:
+			if res['reports'] != []:
+				htmllinks = []
+				for j in res['reports']:
+					if len(j[2]) != 0:
+						## here we should either do a database lookup to get the checksum,
+						## or check if they are already in the report
+						htmllinks.append((j[1], j[2]))
+				if htmllinks != []:
+					uniquehtml = "<html><body><h1>Unique matches per package</h1><p><ul>"
+					## first generate a header
+					for h in htmllinks:
+						uniquehtml = uniquehtml + "<li><a href=\"#%s\">%s (%d)</a>" % (h[0], h[0], len(h[1]))
+					uniquehtml = uniquehtml + "</ul></p>"
+					for h in htmllinks:
+						uniquehtml = uniquehtml + "<hr><h2><a name=\"%s\" href=\"#%s\">Matches for: %s (%d)</a></h2>" % (h[0], h[0], h[0], len(h[1]))
+						for k in h[1]:
+							## we have a list of tuples, per unique string we have a list of sha256sums and meta info
+							## This is really hairy
+							if len(k) > 1:
+								uniquehtml = uniquehtml + "<h5>%s</h5><p><table><tr><td><b>Filename</b></td><td><b>Version(s)</b></td><td><b>Line number</b></td><td><b>SHA256</b></td></tr>" % cgi.escape(k[0])
+								uniqtablerows = []
+								sh = {}
+								for s in k[1]:
+									## if possible, remove the package name, plus version number, from the path
+									## that is displayed. This is to prevent that a line is printed for every
+									## version, even when the code has not changed. Usually it will be clear
+									## which file is meant.
+									(pv, fp) = s[3].split('/', 1)
+									## clean up some names first, especially when they have been changed by Debian
+									for e in ["+dfsg", "~dfsg", ".orig", ".dfsg1", ".dfsg2"]:
+										if pv.endswith(e):
+											pv = pv[:-len(e)]
+											break
+									if pv == "%s-%s" % (h[0], s[1]) or pv == "%s_%s" % (h[0], s[1]):
+										if sh.has_key(s[0]):
+											sh[s[0]].append((fp,s[1], s[2]))
+										else:
+											sh[s[0]] = [(fp, s[1], s[2])]
+									else:	
+										if sh.has_key(s[0]):
+											sh[s[0]].append((s[3],s[1], s[2]))
+										else:
+											sh[s[0]] = [(s[3], s[1], s[2])]
+								for s in sh:
+									## per checksum we have a list of (filename, version)
+									## Now we need to check if we only have one filename, or if there are multiple.
+									## If there is just one it is easy:
+									if len(set(map(lambda x: x[0], sh[s]))) == 1:
+										lines = sorted(set(map(lambda x: (x[2]), sh[s])))
+										versions = sorted(set(map(lambda x: (x[1]), sh[s])))
 										versionline = squash_versions(versions)
 										numlines = reduce(lambda x, y: x + ", " + y, map(lambda x: "<a href=\"unique:/%s#%d\">%d</a>" % (s, x, x), lines))
-										uniqtablerows.append("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n" % (d, versionline, numlines, s))
-							uniquehtml = uniquehtml + reduce(lambda x, y: x + y, uniqtablerows, "") + "</table></p>\n"
-						else:
-							uniquehtml = uniquehtml + "<h5>%s</h5>" % cgi.escape(k[0])
-				uniquehtml = uniquehtml + "</body></html>"
-				uniquehtmlfile = gzip.open("%s/%s-unique.html.gz" % (reportdir, unpackreport['sha256']), 'wb')
-				uniquehtmlfile.write(uniquehtml)
-				uniquehtmlfile.close()
+										uniqtablerows.append("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n" % (sh[s][0][0], versionline, numlines, s))
+									else:
+										for d in list(set(map(lambda x: x[0], sh[s]))):
+											filterd = filter(lambda x: x[0] == d, sh[s])
+											lines = sorted(set(map(lambda x: (x[2]), filterd)))
+	
+											versions = sorted(set(map(lambda x: (x[1]), filterd)))
+											versionline = squash_versions(versions)
+											numlines = reduce(lambda x, y: x + ", " + y, map(lambda x: "<a href=\"unique:/%s#%d\">%d</a>" % (s, x, x), lines))
+											uniqtablerows.append("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n" % (d, versionline, numlines, s))
+								uniquehtml = uniquehtml + reduce(lambda x, y: x + y, uniqtablerows, "") + "</table></p>\n"
+							else:
+								uniquehtml = uniquehtml + "<h5>%s</h5>" % cgi.escape(k[0])
+					uniquehtml = uniquehtml + "</body></html>"
+					uniquehtmlfile = gzip.open("%s/%s-unique.html.gz" % (reportdir, unpackreport['sha256']), 'wb')
+					uniquehtmlfile.write(uniquehtml)
+					uniquehtmlfile.close()
+	
+			if res['unmatched'] != []:
+				unmatches = list(set(res['unmatched']))
+				unmatches.sort()
+				unmatchedhtml = "<html><body><h1>Unmatched strings for %s</h1><p><ul>" % filename
+				for i in unmatches:
+					unmatchedhtml = unmatchedhtml + "%s<br>\n" % cgi.escape(i)
+				unmatchedhtml = unmatchedhtml + "</body></html>"
+				unmatchedhtmlfile = gzip.open("%s/%s-unmatched.html.gz" % (reportdir, unpackreport['sha256']), 'wb')
+				unmatchedhtmlfile.write(unmatchedhtml)
+				unmatchedhtmlfile.close()
 
-		if res['unmatched'] != []:
-			unmatches = list(set(res['unmatched']))
-			unmatches.sort()
-			unmatchedhtml = "<html><body><h1>Unmatched strings for %s</h1><p><ul>" % filename
-			for i in unmatches:
-				unmatchedhtml = unmatchedhtml + "%s<br>\n" % cgi.escape(i)
-			unmatchedhtml = unmatchedhtml + "</body></html>"
-			unmatchedhtmlfile = gzip.open("%s/%s-unmatched.html.gz" % (reportdir, unpackreport['sha256']), 'wb')
-			unmatchedhtmlfile.write(unmatchedhtml)
-			unmatchedhtmlfile.close()
+
+		if variablepvs == {} and dynamicRes == {}:
+			return
+
+		if dynamicRes != {}:
+			header = "<html><body>"
+			html = ""
+			if dynamicRes.has_key('uniquepackages'):
+				if dynamicRes['uniquepackages'] != {}:
+					html += "<h1>Unique function name matches per package</h1><p><ul>\n"
+					ukeys = map(lambda x: (x[0], len(x[1])), dynamicRes['uniquepackages'].items())
+					ukeys.sort(key=lambda x: x[1], reverse=True)
+					for i in ukeys:
+						html += "<li><a href=\"#%s\">%s (%d)</a>" % (i[0], i[0], i[1])
+					html += "</ul></p>"
+					for i in ukeys:
+						html += "<hr><h2><a name=\"%s\" href=\"#%s\">Matches for %s (%d)</a></h2><p>\n" % (i[0], i[0], i[0], i[1])
+						upkgs = dynamicRes['uniquepackages'][i[0]]
+						upkgs.sort()
+						for v in upkgs:
+							html += "%s<br>\n" % v
+						html += "</p>\n"
+			footer = "</body></html>"
+			if html != "":
+				html = header + html + footer
+				nameshtmlfile = gzip.open("%s/%s-functionnames.html.gz" % (reportdir, unpackreport['sha256']), 'wb')
+				nameshtmlfile.write(html)
+				nameshtmlfile.close()
+		if variablepvs != {}:
+			header = "<html><body>"
+			html = ""
+			language = variablepvs['language']
+
+			if language == 'Java':
+				fieldspackages = {}
+				sourcespackages = {}
+				classespackages = {}
+				fieldscount = {}
+				sourcescount = {}
+				classescount = {}
+				for i in ['classes', 'sources', 'fields']:
+					if not variablepvs.has_key(i):
+						continue
+					packages = {}
+					packagecount = {}
+					if variablepvs[i] != []:
+						for c in variablepvs[i]:
+							lenres = len(list(set(map(lambda x: x[0], variablepvs[i][c]))))
+							if lenres == 1:
+								pvs = variablepvs[i][c]
+								(package,version) = variablepvs[i][c][0]
+								if packagecount.has_key(package):
+									packagecount[package] = packagecount[package] + 1
+								else:
+									packagecount[package] = 1
+								'''
+								## for later use
+								for p in pvs:
+									(package,version) = p
+									if packages.has_key(package):
+										packages[package].append(version)
+									else:
+										packages[package] = [version]
+								'''
+					if packagecount != {}:
+						if i == 'classes':
+							classescount = packagecount
+						if i == 'sources':
+							sourcescount = packagecount
+						if i == 'fields':
+							fieldscount = packagecount
+	
+					if packages != {}:
+						if i == 'classes':
+							classespackages = packages
+						if i == 'sources':
+							sourcespackages = packages
+						if i == 'fields':
+							fieldspackages = packages
+	
+				if classescount != {}:
+					html = html + "<h3>Unique matches of class names</h3>\n<table>\n"
+					html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+					for i in classescount:
+						html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, classescount[i])
+					html = html + "</table>\n"
+	
+				if sourcescount != {}:
+					html = html + "<h3>Unique matches of source file names</h3>\n<table>\n"
+					html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+					for i in sourcescount:
+						html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, sourcescount[i])
+					html = html + "</table>\n"
+	
+				if fieldscount != {}:
+					html = html + "<h3>Unique matches of field names</h3>\n<table>\n"
+					html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+					for i in fieldscount:
+						html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, fieldscount[i])
+					html = html + "</table>\n"
+	
+			if language == 'C':
+				if variablepvs.has_key('variables'):
+					packages = {}
+					packagecount = {}
+					## for each variable name determine in how many packages it can be found.
+					## Only the unique packages are reported.
+					for c in variablepvs['variables']:
+						lenres = len(variablepvs['variables'][c])
+						if lenres == 1:
+							pvs = variablepvs['variables'][c]
+							package = variablepvs['variables'][c].keys()[0]
+							if packagecount.has_key(package):
+								packagecount[package] = packagecount[package] + 1
+							else:
+								packagecount[package] = 1
+								
+						'''
+						## for later use
+						for p in pvs:
+							(package,version) = p
+							if packages.has_key(package):
+								packages[package].append(version)
+							else:
+								packages[package] = [version]
+						'''
+	
+					if packagecount != {}:
+						html = html + "<h3>Unique matches of variables</h3>\n<table>\n"
+						html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+						for i in packagecount:
+							html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, packagecount[i])
+						html = html + "</table>\n"
+	
+			footer = "</body></html>"
+			if html != "":
+				html = header + html + footer
+				nameshtmlfile = gzip.open("%s/%s-names.html.gz" % (reportdir, unpackreport['sha256']), 'wb')
+				nameshtmlfile.write(html)
+				nameshtmlfile.close()
