e7f766418ceb11b24591bd647d284127d4d10393
Armijn Hemel <armijn@tjaldur.nl>
2013-02-20 16:59:54 +0000

    carve blacklisted parts from file first

--- src/bat/busyboxversion.py
+++ src/bat/busyboxversion.py
@@ -7,7 +7,7 @@
 ## Stand alone module to determine the version of BusyBox. Has a method for being called
 ## from one of the default scans, but can also be invoked separately.
 
-import sys, os
+import sys, os, tempfile
 from optparse import OptionParser
 import busybox, extractor
 
@@ -15,9 +15,36 @@ def busybox_version(filename, blacklist=[], envvars=None):
 	try:
                 filesize = os.stat(filename).st_size
 		## if the whole file is blacklisted, we don't have to scan
-                if extractor.inblacklist(0, blacklist) == filesize:
-                        return None
-		bbres = busybox.extract_version(filename)
+		if blacklist != []:
+                	if extractor.inblacklist(0, blacklist) == filesize:
+				return None
+			datafile = open(filename, 'rb')
+			lastindex = 0
+			databytes = ""
+			datafile.seek(lastindex)
+			for i in blacklist:
+				if i[0] == lastindex:
+					lastindex = i[1] - 1
+					datafile.seek(lastindex)
+					continue
+				if i[0] > lastindex:
+					## just concatenate the bytes
+					data = datafile.read(i[0] - lastindex)
+					databytes = databytes + data
+					## set lastindex to the next
+					lastindex = i[1] - 1
+					datafile.seek(lastindex)
+			datafile.close()
+			if len(databytes) == 0:
+				return None
+			tmpfile = tempfile.mkstemp()
+			os.write(tmpfile[0], databytes)
+			os.fdopen(tmpfile[0]).close()
+			scanfile = tmpfile[1]
+			bbres = busybox.extract_version(scanfile)
+			os.unlink(tmpfile[1])
+		else:
+			bbres = busybox.extract_version(filename)
 		if bbres != None:
 			return (['busybox'], bbres)
 	except Exception, e:
