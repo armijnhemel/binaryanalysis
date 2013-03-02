7658813ec316f758ef6f37ca63722775abaccb1f
Armijn Hemel <armijn@tjaldur.nl>
2013-03-02 19:51:26 +0000

    extract more pickles in parallel

--- src/bat/generatereports.py
+++ src/bat/generatereports.py
@@ -4,6 +4,24 @@
 ## Copyright 2013 Armijn Hemel for Tjaldur Software Governance Solutions
 ## Licensed under Apache 2.0, see LICENSE file for details
 
+'''
+This plugin is used to generate reports. It is run as an aggregate scan for
+a reason: as it turns out many reports that are generated are identical:
+matched strings are often the same since the same database is used.
+
+Since generating reports can have quite a bit of overhead it makes sense
+to first deduplicate and then generate reports.
+
+The method works as follows:
+
+1. All data from pickles that is needed to generate reports is extracted in
+parallel.
+2. The checksum of the pickle is computed and recorded. If there is a duplicate
+the pickle is removed and it is recorded which file it originally belonged to.
+3. Reports are (partially) generated in parallel for the remaining pickle files.
+4. The reports are copied and renamed, or assembled from partial reports
+'''
+
 import os, os.path, sys, subprocess, copy, cPickle, tempfile, hashlib, shutil, multiprocessing, cgi, gzip
 
 ## compute a SHA256 hash. This is done in chunks to prevent a big file from
@@ -19,8 +37,42 @@ def gethash(path):
 	scanfile.close()
 	return h.hexdigest()
 
-def generatehtmlsnippet(()):
-	pass
+#def generatehtmlsnippet((picklefile, pickledir, filehash, reportdir)):
+	#pass
+
+def extractpickles((filehash, pickledir, topleveldir)):
+	leaf_file = open(os.path.join(topleveldir, "filereports", "%s-filereport.pickle" % filehash), 'rb')
+	leafreports = cPickle.load(leaf_file)
+	leaf_file.close()
+	## return type: (filehash, reportresults, unmatchedresult)
+	reportresults = []
+
+	## (picklehash, picklename)
+	unmatchedresult = None
+	if leafreports.has_key('ranking'):
+		## the ranking result is (res, dynamicRes, variablepvs)
+		(res, dynamicRes, variablepvs) = leafreports['ranking']
+
+		if res['unmatched'] != []:
+			unmatches = list(set(res['unmatched']))
+			unmatches.sort()
+
+			tmppickle = tempfile.mkstemp()
+
+			cPickle.dump(unmatches, os.fdopen(tmppickle[0], 'w'))
+			picklehash = gethash(tmppickle[1])
+			unmatchedresult = (picklehash, tmppickle[1])
+
+		if res['reports'] != []:
+			for j in res['reports']:
+				(rank, packagename, uniquematches, percentage, packageversions, licenses) = j
+				tmppickle = tempfile.mkstemp()
+				cPickle.dump((packagename, uniquematches), os.fdopen(tmppickle[0], 'w'))
+				picklehash = gethash(tmppickle[1])
+				reportresults.append((picklehash, tmppickle[1]))
+				## TODO: find out what to do with rank and percentage
+
+	return (filehash, reportresults, unmatchedresult)
 
 def generateunmatched((picklefile, pickledir, filehash, reportdir)):
 
@@ -91,70 +143,59 @@ def generatereports(unpackreports, scantempdir, topleveldir, envvars=None):
 	unmatchedpickles = []
 	reportpickles = []
 
-	for r in rankingfiles:
-		filehash = unpackreports[r]['sha256']
-		if filehash in processed:
+	filehashes = list(set(map(lambda x: unpackreports[x]['sha256'], rankingfiles)))
+
+	## extract pickles
+	extracttasks = map(lambda x: (x, pickledir, topleveldir), filehashes)
+	pool = multiprocessing.Pool()
+	res = filter(lambda x: x != None, pool.map(extractpickles, extracttasks))
+	pool.terminate()
+
+	for r in res:
+		(filehash, resultreports, unmatchedresult) = r
+		if r == None:
 			continue
-
-		leaf_file = open(os.path.join(topleveldir, "filereports", "%s-filereport.pickle" % filehash), 'rb')
-		leafreports = cPickle.load(leaf_file)
-		leaf_file.close()
-
-		if leafreports.has_key('ranking'):
-			## the ranking result is (res, dynamicRes, variablepvs)
-			(res, dynamicRes, variablepvs) = leafreports['ranking']
-
-			## process 'unmatched'
-			if res['unmatched'] != []:
-				unmatches = list(set(res['unmatched']))
-				unmatches.sort()
-
-				tmppickle = tempfile.mkstemp()
-
-				cPickle.dump(unmatches, os.fdopen(tmppickle[0], 'w'))
-				picklehash = gethash(tmppickle[1])
-				if picklehash in unmatchedpickles:
-					if pickletofile.has_key(picklehash):
-						pickletofile[picklehash].append(filehash)
-					else:
-						pickletofile[picklehash] = [filehash]
-					unmatchedpicklespackages.append((picklehash, filehash))
-					os.unlink(tmppickle[1])
+		if unmatchedresult != None:
+			(picklehash, tmppickle) = unmatchedresult
+			if picklehash in unmatchedpickles:
+				if pickletofile.has_key(picklehash):
+					pickletofile[picklehash].append(filehash)
 				else:
-					shutil.move(tmppickle[1], pickledir)
-					unmatchedpickles.append(picklehash)
-					unmatchedpicklespackages.append((picklehash, filehash))
-					picklehashes[picklehash] = os.path.basename(tmppickle[1])
+					pickletofile[picklehash] = [filehash]
+				unmatchedpicklespackages.append((picklehash, filehash))
+				os.unlink(tmppickle)
+			else:
+				shutil.move(tmppickle, pickledir)
+				unmatchedpickles.append(picklehash)
+				unmatchedpicklespackages.append((picklehash, filehash))
+				picklehashes[picklehash] = os.path.basename(tmppickle)
+				if pickletofile.has_key(picklehash):
+					pickletofile[picklehash].append(filehash)
+				else:
+					pickletofile[picklehash] = [filehash]
+		if resultreports != []:
+			for report in resultreports:
+				(picklehash, tmppickle) = report
+				if picklehash in reportpickles:
+					if pickletofile.has_key(picklehash):
+						pickletofile[picklehash].append(filehash)
+					else:
+						pickletofile[picklehash] = [filehash]
+					picklespackages.append((picklehash, filehash))
+					os.unlink(tmppickle)
+				else:
+					shutil.move(tmppickle, pickledir)
+					reportpickles.append(picklehash)
+					picklespackages.append((picklehash, filehash))
+					picklehashes[picklehash] = os.path.basename(tmppickle)
 					if pickletofile.has_key(picklehash):
 						pickletofile[picklehash].append(filehash)
 					else:
 						pickletofile[picklehash] = [filehash]
 
-			if res['reports'] != []:
-				for j in res['reports']:
-					(rank, packagename, uniquematches, percentage, packageversions, licenses) = j
-					tmppickle = tempfile.mkstemp()
-					cPickle.dump((packagename, uniquematches), os.fdopen(tmppickle[0], 'w'))
-					picklehash = gethash(tmppickle[1])
-					if picklehash in reportpickles:
-						if pickletofile.has_key(picklehash):
-							pickletofile[picklehash].append(filehash)
-						else:
-							pickletofile[picklehash] = [filehash]
-						picklespackages.append((picklehash, filehash))
-						os.unlink(tmppickle[1])
-					else:
-						shutil.move(tmppickle[1], pickledir)
-						reportpickles.append(picklehash)
-						picklespackages.append((picklehash, filehash))
-						picklehashes[picklehash] = os.path.basename(tmppickle[1])
-						if pickletofile.has_key(picklehash):
-							pickletofile[picklehash].append(filehash)
-						else:
-							pickletofile[picklehash] = [filehash]
 	pool = multiprocessing.Pool()
 
-	## generate hashes for unmatched strings
+	## generate files for unmatched strings
 	if unmatchedpickles != []:
 		unmatchedtasks = list(set(map(lambda x: (picklehashes[x[0]], pickledir, x[0], reportdir), unmatchedpicklespackages)))
 		results = pool.map(generateunmatched, unmatchedtasks, 1)
