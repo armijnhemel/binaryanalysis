1b185a9cd8fbff687e46a5f036c4d9a92eefb2fb
Armijn Hemel <armijn@tjaldur.nl>
2013-03-04 18:53:23 +0000

    move remaining stuff from generateuniquehtml.py to generatereports.py

--- src/bat/generatereports.py
+++ src/bat/generatereports.py
@@ -134,7 +134,8 @@ def generatehtmlsnippet((picklefile, pickledir, picklehash, reportdir)):
 	uniquehtmlfile.write(uniquehtml)
 	uniquehtmlfile.close()
 
-def extractpickles((filehash, pickledir, topleveldir)):
+## a bit of a misnomer, since this method also generates a few things
+def extractpickles((filehash, pickledir, topleveldir, reportdir)):
 	leaf_file = open(os.path.join(topleveldir, "filereports", "%s-filereport.pickle" % filehash), 'rb')
 	leafreports = cPickle.load(leaf_file)
 	leaf_file.close()
@@ -143,29 +144,168 @@ def extractpickles((filehash, pickledir, topleveldir)):
 
 	## (picklehash, picklename)
 	unmatchedresult = None
-	if leafreports.has_key('ranking'):
-		## the ranking result is (res, dynamicRes, variablepvs)
-		(res, dynamicRes, variablepvs) = leafreports['ranking']
+	if not leafreports.has_key('ranking'):
+		return (filehash, reportresults, unmatchedresult)
+	## the ranking result is (res, dynamicRes, variablepvs)
+	(res, dynamicRes, variablepvs) = leafreports['ranking']
 
-		if res['unmatched'] != []:
-			unmatches = list(set(res['unmatched']))
-			unmatches.sort()
+	if res['unmatched'] != []:
+		unmatches = list(set(res['unmatched']))
+		unmatches.sort()
 
+		tmppickle = tempfile.mkstemp()
+
+		cPickle.dump(unmatches, os.fdopen(tmppickle[0], 'w'))
+		picklehash = gethash(tmppickle[1])
+		unmatchedresult = (picklehash, tmppickle[1])
+
+	if res['reports'] != []:
+		for j in res['reports']:
+			(rank, packagename, uniquematches, percentage, packageversions, licenses) = j
+			if len(uniquematches) == 0:
+				continue
 			tmppickle = tempfile.mkstemp()
-
-			cPickle.dump(unmatches, os.fdopen(tmppickle[0], 'w'))
+			cPickle.dump((packagename, uniquematches), os.fdopen(tmppickle[0], 'w'))
 			picklehash = gethash(tmppickle[1])
-			unmatchedresult = (picklehash, tmppickle[1])
+			reportresults.append((rank, picklehash, tmppickle[1], len(uniquematches), packagename))
 
-		if res['reports'] != []:
-			for j in res['reports']:
-				(rank, packagename, uniquematches, percentage, packageversions, licenses) = j
-				if len(uniquematches) == 0:
+	if dynamicRes != {}:
+		header = "<html><body>"
+		html = ""
+		if dynamicRes.has_key('uniquepackages'):
+			if dynamicRes['uniquepackages'] != {}:
+				html += "<h1>Unique function name matches per package</h1><p><ul>\n"
+				ukeys = map(lambda x: (x[0], len(x[1])), dynamicRes['uniquepackages'].items())
+				ukeys.sort(key=lambda x: x[1], reverse=True)
+				for i in ukeys:
+					html += "<li><a href=\"#%s\">%s (%d)</a>" % (i[0], i[0], i[1])
+				html += "</ul></p>"
+				for i in ukeys:
+					html += "<hr><h2><a name=\"%s\" href=\"#%s\">Matches for %s (%d)</a></h2><p>\n" % (i[0], i[0], i[0], i[1])
+					upkgs = dynamicRes['uniquepackages'][i[0]]
+					upkgs.sort()
+					for v in upkgs:
+						html += "%s<br>\n" % v
+					html += "</p>\n"
+		footer = "</body></html>"
+		if html != "":
+			html = header + html + footer
+			nameshtmlfile = gzip.open("%s/%s-functionnames.html.gz" % (reportdir, filehash), 'wb')
+			nameshtmlfile.write(html)
+			nameshtmlfile.close()
+
+	if variablepvs != {}:
+		header = "<html><body>"
+		html = ""
+		language = variablepvs['language']
+
+		if language == 'Java':
+			fieldspackages = {}
+			sourcespackages = {}
+			classespackages = {}
+			fieldscount = {}
+			sourcescount = {}
+			classescount = {}
+			for i in ['classes', 'sources', 'fields']:
+				if not variablepvs.has_key(i):
 					continue
-				tmppickle = tempfile.mkstemp()
-				cPickle.dump((packagename, uniquematches), os.fdopen(tmppickle[0], 'w'))
-				picklehash = gethash(tmppickle[1])
-				reportresults.append((rank, picklehash, tmppickle[1], len(uniquematches), packagename))
+				packages = {}
+				packagecount = {}
+				if variablepvs[i] != []:
+					for c in variablepvs[i]:
+						lenres = len(list(set(map(lambda x: x[0], variablepvs[i][c]))))
+						if lenres == 1:
+							pvs = variablepvs[i][c]
+							(package,version) = variablepvs[i][c][0]
+							if packagecount.has_key(package):
+								packagecount[package] = packagecount[package] + 1
+							else:
+								packagecount[package] = 1
+							'''
+							## for later use
+							for p in pvs:
+								(package,version) = p
+								if packages.has_key(package):
+									packages[package].append(version)
+								else:
+									packages[package] = [version]
+							'''
+				if packagecount != {}:
+					if i == 'classes':
+						classescount = packagecount
+					if i == 'sources':
+						sourcescount = packagecount
+					if i == 'fields':
+						fieldscount = packagecount
+
+				if packages != {}:
+					if i == 'classes':
+						classespackages = packages
+					if i == 'sources':
+						sourcespackages = packages
+					if i == 'fields':
+						fieldspackages = packages
+
+			if classescount != {}:
+				html = html + "<h3>Unique matches of class names</h3>\n<table>\n"
+				html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+				for i in classescount:
+					html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, classescount[i])
+				html = html + "</table>\n"
+
+			if sourcescount != {}:
+				html = html + "<h3>Unique matches of source file names</h3>\n<table>\n"
+				html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+				for i in sourcescount:
+					html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, sourcescount[i])
+				html = html + "</table>\n"
+
+			if fieldscount != {}:
+				html = html + "<h3>Unique matches of field names</h3>\n<table>\n"
+				html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+				for i in fieldscount:
+					html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, fieldscount[i])
+				html = html + "</table>\n"
+
+		if language == 'C':
+			if variablepvs.has_key('variables'):
+				packages = {}
+				packagecount = {}
+				## for each variable name determine in how many packages it can be found.
+				## Only the unique packages are reported.
+				for c in variablepvs['variables']:
+					lenres = len(variablepvs['variables'][c])
+					if lenres == 1:
+						pvs = variablepvs['variables'][c]
+						package = variablepvs['variables'][c].keys()[0]
+						if packagecount.has_key(package):
+							packagecount[package] = packagecount[package] + 1
+						else:
+							packagecount[package] = 1
+							
+					'''
+					## for later use
+					for p in pvs:
+						(package,version) = p
+						if packages.has_key(package):
+							packages[package].append(version)
+						else:
+							packages[package] = [version]
+					'''
+
+				if packagecount != {}:
+					html = html + "<h3>Unique matches of variables</h3>\n<table>\n"
+					html = html + "<tr><td><b>Name</b></td><td><b>Unique matches</b></td></tr>"
+					for i in packagecount:
+						html = html + "<tr><td>%s</td><td>%d</td></tr>\n" % (i, packagecount[i])
+					html = html + "</table>\n"
+
+		footer = "</body></html>"
+		if html != "":
+			html = header + html + footer
+			nameshtmlfile = gzip.open("%s/%s-names.html.gz" % (reportdir, filehash), 'wb')
+			nameshtmlfile.write(html)
+			nameshtmlfile.close()
 
 	return (filehash, reportresults, unmatchedresult)
 
@@ -241,7 +381,7 @@ def generatereports(unpackreports, scantempdir, topleveldir, envvars=None):
 	filehashes = list(set(map(lambda x: unpackreports[x]['sha256'], rankingfiles)))
 
 	## extract pickles
-	extracttasks = map(lambda x: (x, pickledir, topleveldir), filehashes)
+	extracttasks = map(lambda x: (x, pickledir, topleveldir, reportdir), filehashes)
 	pool = multiprocessing.Pool()
 	res = filter(lambda x: x != None, pool.map(extractpickles, extracttasks))
 	pool.terminate()
