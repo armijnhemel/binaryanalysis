f2b10dfa6a4c94ff5b7e268ba1cd79fc04dffa6d
Armijn Hemel <armijn@tjaldur.nl>
2013-03-15 11:29:51 +0000

    store right type

--- src/maintenance/batchextractprogramstrings.py
+++ src/maintenance/batchextractprogramstrings.py
@@ -657,7 +657,7 @@ def extractstrings((package, version, i, p, language, filehash, ninkaversion)):
 						## stored in a special ELF section __ksymtab_strings
 						if csplit[1] == 'variable':
 							if "EXPORT_SYMBOL" in csplit[4]:
-								cresults.append((csplit[0], int(csplit[2]), i))
+								cresults.append((csplit[0], int(csplit[2]), 'variable'))
 					else:
 						for i in ['function', 'variable']:
 							if csplit[1] == i:
