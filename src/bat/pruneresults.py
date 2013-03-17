3eb8ee8f2eb8e06f82b8323184802531e5113201
Armijn Hemel <armijn@tjaldur.nl>
2013-03-17 13:41:30 +0000

    start on code for pruning results

---
+++ src/bat/pruneresults.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+## Binary Analysis Tool
+## Copyright 2013 Armijn Hemel for Tjaldur Software Governance Solutions
+## Licensed under Apache 2.0, see LICENSE file for details
+
+import os, os.path, sys, subprocess, copy, cPickle, multiprocessing
+
+'''
+This program can be used to optionally prune results of a scan. Sometimes
+results of scans can get very large, for example a scan of a Linux kernel image
+could have thousands of string matches, which can each be found in a few
+hundred kernel source code archives.
+
+By pruning results the amount of noise can be much reduced.
+
+To remove a version A from the set of versions the following conditions have
+to hold:
+
+* there is a minimum amount of results available (20 or 30 seems a good cut off value)
+
+* all strings/variables/function names found in A are found in the most promising
+version
+
+* the amount of strings/variables/function names found in A are significantly
+smaller than the amount in the most promising version (expressed as a maximum
+percentage)
+'''
+
+def pruneresults(unpackreports, scantempdir, topleveldir, envvars=None):
+	scanenv = os.environ.copy()
+	if envvars != None:
+		for en in envvars.split(':'):
+			try:
+				(envname, envvalue) = en.split('=')
+				scanenv[envname] = envvalue
+			except Exception, e:
+				pass
+
+	if not scanenv.has_key('BAT_KEEP_VERSIONS'):
+		## keep all versions
+		return
+	else:
+		keepversions = int(scanenv.get('BAT_KEEP_VERSIONS', 0))
+		if keepversions <= 0:
+			## keep all versions
+			return
+
+	if not scanenv.has_key('BAT_KEEP_MAXIMUM_PERCENTAGE'):
+		## keep all versions
+		return
+	else:
+		keepversions = int(scanenv.get('BAT_KEEP_MAXIMUM_PERCENTAGE', 0))
+		if keepversions == 0:
+			## keep all versions
+			return
+		if keepversions >= 100:
+			## keep all versions
+			return
+
+	rankingfiles = []
+
+	## ignore files which don't have ranking results
+	for i in unpackreports:
+		if not unpackreports[i].has_key('sha256'):
+			continue
+		if not unpackreports[i].has_key('tags'):
+			continue
+		if not 'ranking' in unpackreports[i]['tags']:
+			continue
+		filehash = unpackreports[i]['sha256']
+		if not os.path.exists(os.path.join(topleveldir, "filereports", "%s-filereport.pickle" % filehash)):
+			continue
+		rankingfiles.append(i)
