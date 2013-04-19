c5a2371b37fd7968a2bae58dc5df5fb97d0625a3
Armijn Hemel <armijn@tjaldur.nl>
2013-04-19 11:51:31 +0000

    preemptive version bump

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ import glob
 import os.path
 
 setup(name='bat',
-      version='13.0',
+      version='14.0',
       description='Binary Analysis Tool',
       author='Tjaldur Software Governance Solutions',
       author_email='info@binaryanalysis.org',
