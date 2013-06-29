353841a54084b944e02c39e4e9ec82480186df6e
Armijn Hemel <armijn@tjaldur.nl>
2013-06-29 11:46:50 +0000

    doc fixes

--- src/maintenance/rewritelist.py
+++ src/maintenance/rewritelist.py
@@ -5,6 +5,16 @@
 ## Licensed under Apache 2.0, see LICENSE file for details
 
 '''
+This program can be used to generate a LIST file, like generatelist.py, but
+taking two LIST files as input. The first ('correctedlist') is LIST that has
+corrected input. The second one is a possibly non-corrected list.
+
+The main use case is when the database has to be regenerated (new license
+scanners, better string extraction, and so on), with possibly new input.
+Using dumplist.py the (supposedly) corrected list (for old packages) can be
+extracted from the database. With generatelist.py a new list can be generated
+for the packages. By comparing the two and reusing the corrected results a lot
+of effort can be saved.
 '''
 
 import os, os.path, sys, sqlite3
@@ -57,6 +67,7 @@ def main(argv):
 				listentries.append("%s\t%s\t%s\t%s" % (newfiles[i][0], newfiles[i][1], i, newfiles[i][2]))
 		else:
 			listentries.append("%s\t%s\t%s\t%s" % (newfiles[i][0], newfiles[i][1], i, newfiles[i][2]))
+	listentries.sort()
 	for i in listentries:
 		print i
 if __name__ == "__main__":
