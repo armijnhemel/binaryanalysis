756013fdc63eae95c9a0f510ea27357777114807
Armijn Hemel <armijn@tjaldur.nl>
2013-10-06 21:44:19 +0000

    remove dead code

--- src/bat/piecharts.py
+++ src/bat/piecharts.py
@@ -30,33 +30,3 @@ def generateImages(picklefile, pickledir, filehash, imagedir, pietype):
 	pylab.savefig('%s/%s-%s.png' % (imagedir, filehash, pietype))
 	pylab.gcf().clear()
 	os.unlink(os.path.join(pickledir, picklefile))
-
-'''
-### This is just some dead code that was an attempt at generating better piecharts. It might be used in the future, so keep it here for reference.
-					j_sorted = sorted(j[4], key=lambda x: j[4][x])
-					max_y = j[4][j_sorted[-1]]
-					xvalues = []
-					yvalues = []
-					for v in j_sorted:
-						xvalues.append(v)
-						yvalues.append(j[4][v])
-						print >>sys.stderr, v, j[4][v], j[1], xvalues, yvalues, "max", max_y
-
-					figsize = len(xvalues) * 1.0
-					pylab.gcf().set_size_inches(figsize, 7)
-
-					pylab.xlabel('version')
-					pylab.ylabel('matches')
-					pylab.title("Unique matches for %s" % j[1])
-					## leave some space at the top
-					pylab.gca().set_ylim(top=max_y + 1)
-					x = pylab.arange(len(xvalues))
-					b = pylab.bar(x, yvalues, width=0.6)
-					for bb in b:
-						print >>sys.stderr, bb.get_width(), bb.get_height()
-					## center the text
-					pylab.xticks(x+0.3, xvalues, rotation=270)
-
-					pylab.savefig('%s/%s-%s-version.png' % (imagedir, unpackreport['sha256'], j[1]))
-					pylab.gcf().clear()
-'''
