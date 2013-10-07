5c2c4936a61ecfd3c04dcc631cc33f4a444a1431
Armijn Hemel <armijn@tjaldur.nl>
2013-10-07 20:28:24 +0000

    also optionally remove result pickles

--- src/bat/prunefiles.py
+++ src/bat/prunefiles.py
@@ -4,7 +4,7 @@
 ## Copyright 2013 Armijn Hemel for Tjaldur Software Governance Solutions
 ## Licensed under Apache 2.0, see LICENSE file for details
 
-import os, os.path, sys, subprocess, copy, cPickle, multiprocessing
+import os, os.path, sys
 
 '''
 This method can be used to prune scans, by for example ignoring all graphics files
@@ -23,9 +23,23 @@ def prunefiles(unpackreports, scantempdir, topleveldir, processors, debug=False,
 	if not scanenv.has_key("PRUNE_TAGS"):
 		return
 	prunes = scanenv['PRUNE_TAGS']
-	prunetags = set(prunes.split(':'))
+	prunetags = set(prunes.split(','))
 
-	## TODO: also remove filereport pickles, make it configurable
+	cleanpickles = False
+	if scanenv.get('PRUNE_FILEREPORT_CLEAN', 0) == '1':
+		cleanpickles = True
+
+	cleanfiles = set()
 	for u in unpackreports.keys():
 		if set(unpackreports[u]['tags']).intersection(prunetags) != set():
+			if cleanpickles:
+				filehash = unpackreports[u]['sha256']
+				cleanfiles.add(filehash)
 			del unpackreports[u]
+
+	for filehash in cleanfiles:
+		try:
+			os.unlink(os.path.join(topleveldir, "filereports", "%s-filereport.pickle" % filehash))
+		except Exception, e:
+			print >>sys.stderr, "error removing", filehash, e
+			sys.stderr.flush()
