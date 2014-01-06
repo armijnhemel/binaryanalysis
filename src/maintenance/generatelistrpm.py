f01f9fcd6ecfa27fcc0e72dce5991f557781d803
Armijn Hemel <armijn@tjaldur.nl>
2014-01-06 21:38:09 +0000

    unpack as many files as possible in parallel

--- src/maintenance/generatelistrpm.py
+++ src/maintenance/generatelistrpm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ## Binary Analysis Tool
-## Copyright 2011-2013 Armijn Hemel for Tjaldur Software Governance Solutions
+## Copyright 2011-2014 Armijn Hemel for Tjaldur Software Governance Solutions
 ## Licensed under Apache 2.0, see LICENSE file for details
 
 '''
@@ -11,14 +11,32 @@ This variant is specifically for processing a directory full of SRPM files.
 
 1. files are converted to CPIO archives using rpm2cpio
 2. files are unpacked using cpio
-3. archives (ZIP, tar.gz, tar.bz, tgz, etc.) are moved to a temporary directory. Any patches are put in a special patch
+3. archives (ZIP, tar.gz, tar.bz, tgz, etc.) are moved to a temporary directory. TODO: Any patches are put in a special patch
 directory.
 4. LIST file for temporary directory is created
 '''
 
 import sys, os, os.path, subprocess, tempfile, shutil, stat
 from optparse import OptionParser
-from multiprocessing import Pool
+import multiprocessing
+
+def parallel_unpack((rpmfile, target, copyfiles)):
+	## make a temporary directory
+	cpiodir = tempfile.mkdtemp()
+
+	cpiotmp = tempfile.mkstemp(dir=cpiodir)
+
+	p1 = subprocess.Popen(['rpm2cpio', rpmfile], stdin=subprocess.PIPE, stdout=cpiotmp[0], stderr=subprocess.PIPE, close_fds=True, cwd=cpiodir)
+	(cpiostanout, cpiostanerr) = p1.communicate()
+	os.fsync(cpiotmp[0])
+	os.fdopen(cpiotmp[0]).close()
+
+	p2 = subprocess.Popen(['cpio', '-i', '-d', '--no-absolute-filenames'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True, cwd=cpiodir)
+	(cpiostanout, cpiostanerr) = p2.communicate(open(cpiotmp[1]).read())
+	for f in copyfiles:
+		shutil.copy(os.path.join(cpiodir, f), target)
+		os.chmod("%s/%s" % (target, f), stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR)
+	shutil.rmtree(cpiodir)
 
 ## it's either in the form of:
 ##   package-version.extension
@@ -70,6 +88,10 @@ def generatelist(filedir, origin):
 def unpacksrpm(filedir, target):
 	extensions = [".tar.gz", ".tar.bz2", ".tar.xz", ".tgz", ".tbz2"]
 	files = os.walk(filedir)
+	uniquefiles = set()
+	uniquerpms = set()
+	nonuniquerpms = set()
+	rpm2copyfiles = {}
 	try:
         	while True:
 			i = files.next()
@@ -80,7 +102,7 @@ def unpacksrpm(filedir, target):
 				if res[-1] != 'srpm' and (res[-1] != 'rpm' and res[-2] != 'src'):
 					continue
 				else:
-					p2 = subprocess.Popen(['rpm', '-qpl', "%s/%s" % (i[0], p)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True, cwd=target)
+					p2 = subprocess.Popen(['rpm', '-qpl', "%s/%s" % (i[0], p)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
 					(stanout, stanerr) = p2.communicate()
 					rpmfiles = stanout.strip().rsplit("\n")
 					copyfiles = []
@@ -90,10 +112,10 @@ def unpacksrpm(filedir, target):
 							continue
 						(packageversion, extension) = fsplit
 						if extension in ["tgz", "tbz2"]:
-							#pass
+							copyfiles.append(f)
 							continue
 						elif extension in ["jar", "zip"]:
-							#pass
+							copyfiles.append(f)
 							continue
 						else:
 							try:
@@ -104,27 +126,51 @@ def unpacksrpm(filedir, target):
 								continue
 							else:
 								copyfiles.append(f)
-					## make a temporary directory
-					cpiodir = tempfile.mkdtemp()
-					## copy tarball to target
-					oldcwd = os.getcwd()
-					shutil.copy("%s/%s" % (i[0], p), cpiodir)
-					os.chdir(cpiodir)
-					cpiotmp = tempfile.mkstemp(dir=cpiodir)
-					p1 = subprocess.Popen(['rpm2cpio', "%s/%s" % (cpiodir, p)], stdin=subprocess.PIPE, stdout=cpiotmp[0], stderr=subprocess.PIPE, close_fds=True, cwd=cpiodir)
-					(cpiostanout, cpiostanerr) = p1.communicate()
-					os.fsync(cpiotmp[0])
-					os.fdopen(cpiotmp[0]).close()
-					os.unlink("%s/%s" % (cpiodir, p))
-					p2 = subprocess.Popen(['cpio', '-i', '-d', '--no-absolute-filenames'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True, cwd=cpiodir)
-					(cpiostanout, cpiostanerr) = p2.communicate(open(cpiotmp[1]).read())
+					unique = True
 					for f in copyfiles:
-						shutil.copy(f, target)
-						os.chmod("%s/%s" % (target, f), stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR)
-					shutil.rmtree(cpiodir)
-					os.chdir(oldcwd)
+						if f in uniquefiles:
+							unique = False
+							break
+					if unique:
+						uniquefiles.update(set(copyfiles))
+						uniquerpms.add(os.path.join(i[0], p))
+					else:
+						nonuniquerpms.add(os.path.join(i[0], p))
+					rpm2copyfiles[os.path.join(i[0], p)] = copyfiles
 	except Exception, e:
 		print >>sys.stderr, e
+	## unique RPMs can be unpacked in parallel, non-uniques cannot
+	## first process the unique RPMS in parallel
+	tasks = map(lambda x: (x, target, rpm2copyfiles[x]), uniquerpms)
+	pool = multiprocessing.Pool()
+	pool.map(parallel_unpack, tasks,1)
+	pool.terminate()
+
+	## ... then unpack the non-unique RPMS, possibly overwriting already unpacked data
+	## And yes, probably there is a more efficient way to do this.
+	for n in nonuniquerpms:
+		## first check if for all the 'copyfiles' a file with the same name already exists. If so,
+		## then don't unpack.
+		unique = False
+		for f in rpm2copyfiles[n]:
+			if not os.path.exists(os.path.join(target, f)):
+				unique = True
+				break
+		if not unique:
+			continue
+		## make a temporary directory
+		cpiodir = tempfile.mkdtemp()
+		cpiotmp = tempfile.mkstemp(dir=cpiodir)
+		p1 = subprocess.Popen(['rpm2cpio', n], stdin=subprocess.PIPE, stdout=cpiotmp[0], stderr=subprocess.PIPE, close_fds=True, cwd=cpiodir)
+		(cpiostanout, cpiostanerr) = p1.communicate()
+		os.fsync(cpiotmp[0])
+		os.fdopen(cpiotmp[0]).close()
+		p2 = subprocess.Popen(['cpio', '-i', '-d', '--no-absolute-filenames'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True, cwd=cpiodir)
+		(cpiostanout, cpiostanerr) = p2.communicate(open(cpiotmp[1]).read())
+		for f in rpm2copyfiles[n]:
+			shutil.copy(os.path.join(cpiodir, f), target)
+			os.chmod("%s/%s" % (target, f), stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR)
+		shutil.rmtree(cpiodir)
 	return target
 
 def main(argv):
