f734378099dfc2aca30ffc3efbee227aef869b69
Armijn Hemel <armijn@tjaldur.nl>
2014-02-28 15:54:47 +0000

    remove unused import, update copyright

--- src/bat/bruteforcescan.py
+++ src/bat/bruteforcescan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ## Binary Analysis Tool
-## Copyright 2009-2013 Armijn Hemel for Tjaldur Software Governance Solutions
+## Copyright 2009-2014 Armijn Hemel for Tjaldur Software Governance Solutions
 ## Licensed under Apache 2.0, see LICENSE file for details
 
 '''
@@ -41,7 +41,6 @@ are packed in a tar file.
 
 import sys, os, os.path, magic, hashlib, subprocess, tempfile, shutil, stat, multiprocessing, cPickle, glob, tarfile, copy, gzip, Queue
 from optparse import OptionParser
-import ConfigParser
 import datetime
 import sqlite3
 import extractor
