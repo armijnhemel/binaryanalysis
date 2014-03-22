7c3e46abae1dd7a2fb02e1eede114e58a020a045
Armijn Hemel <armijn@tjaldur.nl>
2014-03-22 22:45:50 +0000

    integrate extractkernelconfigy.py

--- src/maintenance/createdb.py
+++ src/maintenance/createdb.py
@@ -15,6 +15,33 @@ package version filename origin
 separated by whitespace
 
 Compression is determined using magic
+
+Currently the following information is stored in the knowledgebase:
+
+* string constants (all languages)
+* function names (C)
+* variable names (C/Java)
+* class names (Java)
+* method names (Java/C#)
+* licenses (all languages)
+* copyright information (all languages)
+
+For the Linux kernel additional information is extracted:
+
+* symbols
+* module alias
+* module author
+* module description
+* module firmware file names
+* module license
+* module parameters + parameter descriptions
+* module version
+
+This tool extracts configurations from Makefiles and Kconfig files in Linux
+kernels and tries to determine which files are included by a configuration
+directives. This information is useful to try and determine a mapping from a
+binary kernel image and modules back to a configuration.
+
 '''
 
 import sys, os, magic, string, re, subprocess, shutil, stat, datetime
@@ -196,6 +223,290 @@ def splitSpecialChars(s):
 			else:
 				lensplit = len(i)
 	return final_splits
+
+## walk the Linux kernel directory and process all the Makefiles
+def extractkernelconfiguration(kerneldir):
+	kerneldirlen = len(kerneldir)+1
+	osgen = os.walk(kerneldir)
+	makefileresults = []
+	kconfigresults = []
+	moduleresults = []
+
+	try:
+		dirstoconfigs = {}
+		while True:
+                	i = osgen.next()
+			## some top level dirs are not interesting
+			if 'Documentation' in i[1] and i[0][kerneldirlen:] == "":
+				i[1].remove('Documentation')
+			if "scripts" in i[1] and i[0][kerneldirlen:] == "":
+				i[1].remove('scripts')
+			if "usr" in i[1] and i[0][kerneldirlen:] == "":
+				i[1].remove('usr')
+			if "samples" in i[1] and i[0][kerneldirlen:] == "":
+				i[1].remove('samples')
+			for p in i[2]:
+				## only process Makefiles and Kconfig
+				if p != 'Makefile' and not 'Kconfig' in p:
+					continue
+
+				source = open(os.path.join(i[0], p)).readlines()
+
+				if p == 'Makefile':
+					if i[0][kerneldirlen:] == "":
+						continue
+
+					## temporary store
+					tmpconfigs = {}
+
+					continued = False
+
+					## first clean up the Makefile, filter out uninteresting
+					## lines and process line continuations
+					makefile = []
+					storeline = ""
+					for line in source:
+						if not continued:
+							if line.strip().startswith('#'):
+								continue
+							if line.strip().startswith('echo'):
+								continue
+							if line.strip().startswith('@'):
+								continue
+							if line.strip() == "":
+								continue
+						if line.strip().endswith("\\"):
+							## replace \ with a space, then concatenate lines
+							storeline = storeline + line.strip()[:-1] + " "
+							continued = True
+							continue
+						else:
+							storeline = storeline + line.strip()
+							continued = False
+
+						if not continued:
+							if storeline == "":
+								makefile.append(line.strip())
+							else:
+								makefile.append(storeline)
+								storeline = ""
+
+					inif = False
+					iniflevel = 0
+
+					nomatches = []
+
+					for line in makefile:
+						if line.strip().startswith('.PHONY:'):
+							continue
+						if line.strip().startswith('doc:'):
+							continue
+						if line.strip().startswith('cleandoc:'):
+							continue
+						if line.strip().startswith('clean:'):
+							continue
+						if line.strip().startswith('clean-files'):
+							continue
+						# if statements can be nested, so keep track of levels
+						if line.strip() == "endif":
+							inif = False
+							iniflevel = iniflevel -1
+							continue
+						# if statements can be nested, so keep track of levels
+						if re.match("ifn?\w+", line.strip()):
+							inif = True
+							iniflevel = iniflevel +1
+
+						res = re.match("([\w\.]+)\-\$\(CONFIG_(\w+)\)\s*[:+]=\s*([\w\-\.\s/=]*)", line.strip())
+						if res != None:
+							## current issues: ARCH (SH, Xtensa, h8300) is giving some issues
+							if "flags" in res.groups()[0]:
+								continue
+							if "FLAGS" in res.groups()[0]:
+								continue
+							if "zimage" in res.groups()[0]:
+								continue
+							if res.groups()[0] == "defaultimage":
+								continue
+							if res.groups()[0] == "cacheflag":
+								continue
+							if res.groups()[0] == "cpuincdir":
+								continue
+							if res.groups()[0] == "cpuclass":
+								continue
+							if res.groups()[0] == "cpu":
+								continue
+							if res.groups()[0] == "machine":
+								continue
+							if res.groups()[0] == "model":
+								continue
+							if res.groups()[0] == "load":
+								continue
+							if res.groups()[0] == "dataoffset":
+								continue
+							if res.groups()[0] == "entrypoint":
+								continue
+							if res.groups()[0] == "textaddr":
+								continue
+							if res.groups()[0] == "CPP_MODE":
+								continue
+							if res.groups()[0] == "LINK":
+								continue
+							if "=" in res.groups()[2]:
+								continue
+							config = "CONFIG_" + res.groups()[1]
+							files = res.groups()[2].split()
+							for f in files:
+								match = matchconfig(f, i[0], config, kerneldirlen)
+								if match != None:
+									if not f.endswith('.o'):
+										dirpath = os.path.normpath(os.path.join(i[0][kerneldirlen:], f))
+										if dirstoconfigs.has_key(dirpath):
+											dirstoconfigs[dirpath].append(config)
+										else:
+											dirstoconfigs[dirpath] = [config]
+									makefileresults.append(match)
+								else:
+									if f.endswith('.o'):
+										tmpconfigs[f[:-2]] = config
+						else:
+							nomatches.append(line.strip())
+
+					for line in nomatches:
+						res = re.match("([\w\.\-]+)\-objs\s*[:+]=\s*([\w\-\.\s/]*)", line.strip())
+						if res != None:
+							tmpkey = res.groups()[0]
+							tmpvals = res.groups()[1].split()
+							if tmpconfigs.has_key(tmpkey):
+								for f in tmpvals:
+									match = matchconfig(f, i[0], tmpconfigs[tmpkey], kerneldirlen)
+									if match != None:
+										makefileresults.append(match)
+							else:
+								if dirstoconfigs.has_key(os.path.normpath(i[0][kerneldirlen:])):
+									for f in tmpvals:
+										for m in dirstoconfigs[os.path.normpath(i[0][kerneldirlen:])]:
+											match = matchconfig(f, i[0], m, kerneldirlen)
+											if match != None:
+												makefileresults.append(match)
+						else:
+							res = re.match("([\w\.\-]+)\-y\s*[:+]=\s*([\w\-\.\s/=]*)", line.strip())
+							if res != None:
+								tmpkey = res.groups()[0]
+								tmpvals = res.groups()[1].split()
+								if tmpconfigs.has_key(tmpkey):
+									for f in tmpvals:
+										match = matchconfig(f, i[0], tmpconfigs[tmpkey], kerneldirlen)
+										if match != None:
+											makefileresults.append(match)
+											moduleresults.append((os.path.normpath(match[0]),tmpkey))
+				else:
+					configs = []
+					inhelp = False
+					inconfig = False
+					currentconfig = ""
+					configtype = ""
+
+					## menus can be stacked. Inside menus there can be definitions that
+					## apply to all configurations inside the menu.
+					## Files might also have global definitions that apply to every
+					## configuration in the file.
+					menus = []
+					menuconfigs = []
+					globalcfgs = []
+
+					ifcfgs = []
+
+					for line in source:
+						if not (line.startswith(" ") or line.startswith("\t")):
+							inhelp = False
+							inconfig = False
+						if inhelp:
+							continue
+						## ignore comments
+						if line.strip().startswith('#'):
+							continue
+						## ignore empty lines
+						if line.strip() == "":
+							continue
+						## new config starts here. Store the old configuration, with all
+						## its definitions and dependencies.
+						if line.startswith('config '):
+							## sanity check, config line always has just 2
+							## elements, separated by whitespace.
+							if len(line.strip().split()) != 2:
+								continue
+							inconfig = True
+							configdirective = "CONFIG_%s" % line.strip().split()[-1]
+							currentconfig = configdirective
+							continue
+						if line.strip() == '---help---' or line.strip() == 'help':
+							inhelp = True
+							continue
+						if line.strip().startswith('if '):
+							ifcfgs.append([])
+							continue
+						if line.strip().startswith('endif'):
+							ifcfgs.pop()
+							continue
+						if line.strip().startswith('menu '):
+							currentconfig = ""
+							continue
+						if line.strip().startswith('select'):
+							pass
+						## add depends and constraints
+						## These can be configuration specific, menu specific or file wide
+						if line.strip().startswith('depends '):
+							depends = line.strip()
+							if depends[0] == 'on':
+								depends = depends[1:]
+							#print currentconfig, depends
+						if line.strip().startswith('tristate'):
+							configtype = 'tristate'
+							continue
+						if line.strip().startswith('bool'):
+							configtype = 'bool'
+							continue
+						if line.strip().startswith('hex'):
+							configtype = 'hex'
+							continue
+						if line.strip().startswith('int'):
+							configtype = 'int'
+							continue
+						if line.strip().startswith('string'):
+							configtype = 'string'
+							continue
+	except StopIteration:
+		return (makefileresults, kconfigresults, moduleresults)
+
+## helper method for processing information from Linux kernel Makefiles
+## output is a tuple (file/dirname, config) for later processing
+def matchconfig(filename, dirname, config, kerneldirlen):
+	if filename.endswith(".o"):
+		try:
+			os.stat(os.path.join(dirname, filename[:-2] + ".c"))
+			return (os.path.join(dirname[kerneldirlen:], filename[:-2] + ".c"), config)
+		except:
+			pass
+		try:
+			os.stat(os.path.join(dirname, filename[:-2] + ".S"))
+			return (os.path.join(dirname[kerneldirlen:], filename[:-2] + ".S"), config)
+		except:
+			return None
+	else:
+		## first see if the directory is relative to the current directory
+		try:
+			os.stat(os.path.join(dirname, filename))
+			return (os.path.join(dirname[kerneldirlen:], filename), config)
+		except:
+			## then see if it is relative to the top level directory
+			try:
+				os.stat(os.path.join(dirname[:kerneldirlen], filename))
+				return (os.path.join(dirname[kerneldirlen:], filename), config)
+			except:
+				return None
+			else:
+				return None
 
 ## unpack the directories to be scanned. For speed improvements it might be
 ## wise to use a ramdisk or tmpfs for this, although when using Ninka and
@@ -578,9 +889,14 @@ def traversefiletree(srcdir, conn, cursor, package, version, license, copyrights
 	## So this is not robust if one of the databases (say, licenses)
 	## is modified by another tool, or deleted and needs to be
 	## regenerated.
+	addtofiletohash = False
+	if filetohash == {}:
+		addtofiletohash = True
 	for s in scanfile_result:
 		(path, filename, filehash, extension, language) = s
 		insertfiles.add((os.path.join(path[srcdirlen:],filename), filehash))
+		if addtofiletohash:
+			filetohash[os.path.join(path[srcdirlen:],filename)] = filehash
 
 		## if many versions of a single package are processed there is likely going to be
 		## overlap. Avoid hitting the disk by remembering the SHA256 from a previous run.
@@ -702,7 +1018,6 @@ def traversefiletree(srcdir, conn, cursor, package, version, license, copyrights
 		licensecursor.close()
 		licenseconn.close()
 
-
 	## extract copyrights
 	if copyrights:
 		if 'patch' in languages:
@@ -725,6 +1040,28 @@ def traversefiletree(srcdir, conn, cursor, package, version, license, copyrights
 			licenseconn.commit()
 			licensecursor.close()
 			licenseconn.close()
+
+	## extract configuration from the Linux kernel Makefiles
+	## store two things:
+	## 1. if it is a path/subdir, store subdir + configuration
+	##    making it searchable by subdir
+	## 2. if it is an objectfile, store name of source(!) file + configuration
+	##    making it searchable by source file
+	## These can and will overlap
+	if package == 'linux':
+		(makefileresults, kconfigresults, moduleresults) = extractkernelconfiguration(srcdir)
+		filehashtomodule = {}
+		for res in makefileresults:
+			pathstring = res[0]
+			configstring = res[1]
+			cursor.execute('''insert into kernel_configuration(configstring, filename, version) values (?, ?, ?)''', (configstring, pathstring, version))
+		for res in moduleresults:
+			(kernelfilename, modulename) = res
+			if filetohash.has_key(kernelfilename):
+				if filehashtomodule.has_key(filetohash[kernelfilename]):
+					filehashtomodule[filetohash[kernelfilename]].append(modulename)
+				else:
+					filehashtomodule[filetohash[kernelfilename]] = [modulename]
 
 	## process the files to scan in parallel, then process the results
 	extracted_results = pool.map(extractidentifiers, filestoscan, 1)
@@ -1723,6 +2060,12 @@ def main(argv):
 		c.execute('''create index if not exists name_name_index on extracted_name(name)''')
 		c.execute('''create index if not exists name_type_index on extracted_name(type)''')
 		c.execute('''create index if not exists name_language_index on extracted_name(language);''')
+
+		## Store information about Linux kernel configuration directives
+		## TODO: check if this should be changed to use SHA256 instead of file names.
+		## TODO: add whether or not a configuration - filename mapping is 1:1
+		c.execute('''create table if not exists kernel_configuration(configstring text, filename text, version text)''')
+		c.execute('''create index if not exists kernel_configuration_filename on kernel_configuration(filename)''')
 
 		## Store information about Linux kernel modules
 		c.execute('''create table if not exists kernelmodule_alias(sha256 text, modulename text, alias text)''')
