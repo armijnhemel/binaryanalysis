3d6f484b78e218d2f9897ff16d1f3fb686c3f6da
Armijn Hemel <armijn@tjaldur.nl>
2015-01-25 19:18:23 +0000

    cleanup

--- src/maintenance/scorecache.py
+++ src/maintenance/scorecache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ## Binary Analysis Tool
-## Copyright 2014 Armijn Hemel for Tjaldur Software Governance Solutions
+## Copyright 2014-2015 Armijn Hemel for Tjaldur Software Governance Solutions
 ## Licensed under Apache 2.0, see LICENSE file for details
 
 import sys, os, os.path
@@ -20,21 +20,21 @@ def main(argv):
 		print >>sys.stderr, "Caching database %s does not exist" % options.db
 		sys.exit(1)
 
-	conn3 = sqlite3.connect(options.db)
-	c3 = conn3.cursor()
+	conn = sqlite3.connect(options.db)
+	c = conn3.cursor()
 
-	c3.execute("create table if not exists scores (stringidentifier text, packages int, score real)")
-	c3.execute("create index if not exists scoresindex on scores(stringidentifier)")
-	conn3.commit()
+	c.execute("create table if not exists scores (stringidentifier text, packages int, score real)")
+	c.execute("create index if not exists scoresindex on scores(stringidentifier)")
+	conn.commit()
 
-	c3.execute("select distinct stringidentifier from stringscache")
-	programstrings = c3.fetchall()
+	c.execute("select distinct stringidentifier from stringscache")
+	programstrings = c.fetchall()
 	for p in programstrings:
 		pkgs = {}
 		filenames = {}
 
-		pfs = c3.execute("select package, filename from stringscache where stringidentifier=?", p).fetchall()
-		packages = list(set(map(lambda x: x[0], pfs)))
+		pfs = c.execute("select package, filename from stringscache where stringidentifier=?", p).fetchall()
+		packages = set(map(lambda x: x[0], pfs))
 
 		if len(packages) == 1:
 			score = float(len(p[0]))
@@ -49,9 +49,12 @@ def main(argv):
 				score = len(p[0]) / pow(alpha, (len(filenames) - 1))
 			except Exception, e:
 				score = len(p[0]) / sys.maxint
-		c3.execute("insert into scores(stringidentifier, packages, score) values (?,?,?)", (p[0], len(packages), float(score)))
-	c3.execute("vacuum")
-	conn3.commit()
+		c.execute("insert into scores(stringidentifier, packages, score) values (?,?,?)", (p[0], len(packages), float(score)))
+	print "vacuuming"
+	c.execute("vacuum")
+	conn.commit()
+	c.close()
+	conn.close()
 	
 if __name__ == "__main__":
 	main(sys.argv)
