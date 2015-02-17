dc526cf01757f0cf00e8608b32ab37c209a868b2
Armijn Hemel <armijn@tjaldur.nl>
2015-02-17 19:47:49 +0000

    add a lot of code for processing RPM spec files. This is not functional yet

--- src/maintenance/generatelistrpm.py
+++ src/maintenance/generatelistrpm.py
@@ -17,9 +17,10 @@ directory.
 5. LIST file for temporary directory is created
 '''
 
-import sys, os, os.path, subprocess, tempfile, shutil, stat, sqlite3
+import sys, os, os.path, subprocess, tempfile, shutil, stat, sqlite3, re
 from optparse import OptionParser
 import multiprocessing
+import hashlib
 
 ## spec file scanner to process any patches that are actually applied
 ## extract the following:
@@ -30,21 +31,98 @@ import multiprocessing
 ## * any applied patches
 ## * any unapplied patches
 ## * possibly license and URL
-def scanspec(specfile):
+## TODO: also process if, undefine, etc. and verify what patches exist
+def scanspec(specfile, specdir):
 	result = {}
 	patches = {}
 	appliedpatches = set()
-	speclines = map(lambda x: x.strip(), open(specfile, 'r').readlines())
-	for s in speclines:
+	speclines = map(lambda x: x.rstrip(), open(os.path.join(specdir, specfile), 'r').readlines())
+	defines = {}
+	globaldefines = {}
+	defines['nil'] = ''
+	globaldefines['nil'] = ''
+	unresolvedpatches = set()
+	missingpatches = set()
+	filelist = os.listdir(specdir)
+	for line in speclines:
 		if line.startswith('Name:'):
-			pass
+			name = line.split(':',1)[1].strip()
+			if not '%{' in name:
+				result['name'] = name
+				continue
+			specreplaces = re.findall("%{([\w\d]+)}", name)
+			for s in specreplaces:
+				if s in result:
+					name = name.replace("%{" + s + "}", result[s])
+				elif s in defines:
+					name = name.replace("%{" + s + "}", defines[s])
+				elif s in globaldefines:
+					name = name.replace("%{" + s + "}", globaldefines[s])
+			if not '%{'in name:
+				result['name'] = name
+		elif line.startswith('%define'):
+			definesplit = line.strip()[1:].split()
+			if len(definesplit) != 3:
+				continue
+			if '%' in definesplit[1]:
+				continue
+			specreplaces = re.findall("%{([\w\d]+)}", definesplit[2])
+			for s in specreplaces:
+				if s in globaldefines:
+					definesplit[2] = definesplit[2].replace("%{" + s + "}", globaldefines[s])
+				elif s in defines:
+					definesplit[2] = definesplit[2].replace("%{" + s + "}", defines[s])
+			if '%' in definesplit[2]:
+				continue
+			defines[definesplit[1]] = definesplit[2]
+		elif line.startswith('%global'):
+			definesplit = line.strip()[1:].split()
+			if len(definesplit) != 3:
+				continue
+			if '%' in definesplit[1]:
+				continue
+			specreplaces = re.findall("%{([\w\d]+)}", definesplit[2])
+			for s in specreplaces:
+				if s in globaldefines:
+					definesplit[2] = definesplit[2].replace("%{" + s + "}", globaldefines[s])
+				elif s in result:
+					definesplit[2] = definesplit[2].replace("%{" + s + "}", result[s])
+			if '%' in definesplit[2]:
+				continue
+			globaldefines[definesplit[1]] = definesplit[2]
 		elif line.startswith('Release:'):
 			pass
 		elif line.startswith('Version:'):
-			pass
+			if 'version' in result:
+				continue
+			version = line.split(':',1)[1].strip()
+			if not '%{' in version:
+				result['version'] = version
+			specreplaces = re.findall("%{([\w\d]+)}", version)
+			for s in specreplaces:
+				if s in result:
+					version = version.replace("%{" + s + "}", result[s])
+				elif s in defines:
+					version = version.replace("%{" + s + "}", defines[s])
+				elif s in globaldefines:
+					version = version.replace("%{" + s + "}", globaldefines[s])
+			if not ('%{' in version or '%(' in version):
+				result['version'] = version
 		elif line.startswith('URL:'):
 			url = line.split(':',1)[1].strip()
-			result['url'] = url
+			if not '%{' in url:
+				result['url'] = url
+				continue
+			specreplaces = re.findall("%{([\w\d]+)}", url)
+			for s in specreplaces:
+				if s in result:
+					url = url.replace("%{" + s + "}", result[s])
+				elif s in defines:
+					url = url.replace("%{" + s + "}", defines[s])
+				elif s in globaldefines:
+					url = url.replace("%{" + s + "}", globaldefines[s])
+			if not '%{' in url:
+				result['url'] = url
 		elif line.startswith('License:'):
 			license = line.split(':',1)[1].strip()
 			result['license'] = license
@@ -55,12 +133,56 @@ def scanspec(specfile):
 			pass
 		elif line.startswith('Patch'):
 			patchsplit = line.split(':', 1)
-			patches[patchsplit[0].lower()] = patchsplit[1]
+			if len(patchsplit) == 1:
+				continue
+			if re.match('Patch\d+', patchsplit[0]) == None:
+				if not line.startswith('Patch:'):
+					continue
+			patchname = patchsplit[1].strip()
+			if "%{" in patchname:
+				specreplaces = re.findall("%{([\w\d_?]+)}", patchname)
+				for s in specreplaces:
+					optional = False
+					if s.startswith('?'):
+						s = s[1:]
+						optional = True
+					if not s in patchname:
+						continue
+					if s in result:
+						if optional:
+							patchname = patchname.replace("%{?" + s + "}", result[s])
+						else:
+							patchname = patchname.replace("%{" + s + "}", result[s])
+					elif s in defines:
+						if optional:
+							patchname = patchname.replace("%{?" + s + "}", defines[s])
+						else:
+							patchname = patchname.replace("%{" + s + "}", defines[s])
+					elif s in globaldefines:
+						if optional:
+							patchname = patchname.replace("%{?" + s + "}", globaldefines[s])
+						else:
+							patchname = patchname.replace("%{" + s + "}", globaldefines[s])
+			patches[patchsplit[0].lower()] = os.path.basename(patchname)
 		elif line.startswith('%patch'):
-			## check if patch is known. If so, apply it
-			appliedpatch = line[1:].split('', 1)[0]
+			## check if patch is known. If so, add it to the applied patches
+			appliedpatch = line[1:].split(' ', 1)[0]
 			if appliedpatch in patches:
-				appliedpatches.add(patches[appliedpatch]
+				if '%{' in patches[appliedpatch]:
+					unresolvedpatches.add(patches[appliedpatch])
+					## if it is unresolved try to do a fuzzy match
+					## * split patches[appliedpatch] and look in filelist
+				else:
+					if os.path.exists(os.path.join(specdir, patches[appliedpatch])):
+						appliedpatches.add(patches[appliedpatch])
+					else:
+						missingpatches.add(patches[appliedpatch])
+	if len(appliedpatches) != 0:
+		result['appliedpatches'] = appliedpatches
+	if len(unresolvedpatches) != 0:
+		result['unresolvedpatches'] = unresolvedpatches
+	if len(missingpatches) != 0:
+		result['missingpatches'] = missingpatches
 	return result
 
 def parallel_unpack((rpmfile, target, copyfiles, unpacktmpdir, cutoff)):
@@ -75,17 +197,28 @@ def parallel_unpack((rpmfile, target, copyfiles, unpacktmpdir, cutoff)):
 
 	cpiotmp = tempfile.mkstemp(dir=cpiodir)
 
-	p1 = subprocess.Popen(['rpm2cpio', rpmfile], stdin=subprocess.PIPE, stdout=cpiotmp[0], stderr=subprocess.PIPE, close_fds=True, cwd=cpiodir)
+	p1 = subprocess.Popen(['rpm2cpio', rpmfile], stdin=subprocess.PIPE, stdout=cpiotmp[0], stderr=subprocess.PIPE, cwd=cpiodir)
 	(cpiostanout, cpiostanerr) = p1.communicate()
 	os.fsync(cpiotmp[0])
 	os.fdopen(cpiotmp[0]).close()
 
-	p2 = subprocess.Popen(['cpio', '-i', '-d', '--no-absolute-filenames', '-F', cpiotmp[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True, cwd=cpiodir)
+	p2 = subprocess.Popen(['cpio', '-i', '-d', '--no-absolute-filenames', '-F', cpiotmp[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cpiodir)
 	(cpiostanout, cpiostanerr) = p2.communicate()
+	os.unlink(cpiotmp[1])
+	## first analyse the spec file
+	res = []
+	'''
+	unpackedfiles = os.listdir(cpiodir)
+	specfiles = filter(lambda x: x.endswith('.spec'), unpackedfiles)
+	for f in specfiles:
+		res.append(scanspec(f, cpiodir))
+	'''
+	## copy the source code files
 	for f in copyfiles:
 		shutil.copy(os.path.join(cpiodir, f), target)
 		os.chmod(os.path.join(target, f), stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR)
 	shutil.rmtree(cpiodir)
+	return res
 
 ## it's either in the form of:
 ##   package-version.extension
@@ -138,8 +271,8 @@ def generatelist(filedir, origin):
 ## This check is based on conventions on how source code archives are named and
 ## might miss things.
 ## TODO: collect patches as well
+## TODO: extract the spec file
 def scanrpm((filedir, filepath)):
-	extensions = [".tar.gz", ".tar.bz2", ".tar.bz", ".tar.xz", ".tgz", ".tbz2", ".tar.Z", "tar.lz", "tar.lzma"]
 	p2 = subprocess.Popen(['rpm', '-qpl', '--dump', "%s/%s" % (filedir, filepath)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
 	(stanout, stanerr) = p2.communicate()
 	rpmfiles = stanout.strip().rsplit("\n")
@@ -168,7 +301,7 @@ def scanrpm((filedir, filepath)):
 				copyfiles.append((f,md5sum))
 	return (filedir, filepath, copyfiles)
 
-def unpacksrpm(filedir, target, unpacktmpdir):
+def unpacksrpm(filedir, target, unpacktmpdir, rpmdatabase):
 	files = os.walk(filedir)
 	uniquefiles = set()
 	uniquerpms = set()
@@ -187,7 +320,7 @@ def unpacksrpm(filedir, target, unpacktmpdir):
 				if len(res) != 3:
 					continue
 				if res[-1] != 'srpm' and (res[-1] != 'rpm' and res[-2] != 'src'):
-					ccontinue
+					continue
 				else:
 					rpmscans.add((i[0], p))
 	except Exception, e:
@@ -226,8 +359,10 @@ def unpacksrpm(filedir, target, unpacktmpdir):
 	## TODO: make configurable
 	cutoff = 209715200
 	tasks = map(lambda x: (x, target, rpm2copyfiles[x], unpacktmpdir, cutoff), uniquerpms)
-	pool.map(parallel_unpack, tasks,1)
+	res = pool.map(parallel_unpack, tasks,1)
 	pool.terminate()
+	for r in res:
+		print r
 
 	## ... then unpack the non-unique RPMS, possibly overwriting already unpacked data
 	## And yes, probably there is a more efficient way to do this.
@@ -248,8 +383,9 @@ def unpacksrpm(filedir, target, unpacktmpdir):
 		(cpiostanout, cpiostanerr) = p1.communicate()
 		os.fsync(cpiotmp[0])
 		os.fdopen(cpiotmp[0]).close()
-		p2 = subprocess.Popen(['cpio', '-i', '-d', '--no-absolute-filenames', '-F', cpiotmp[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True, cwd=cpiodir)
+		p2 = subprocess.Popen(['cpio', '-i', '-d', '--no-absolute-filenames', '-F', cpiotmp[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cpiodir)
 		(cpiostanout, cpiostanerr) = p2.communicate()
+		os.unlink(cpiotmp[1])
 		for f in rpm2copyfiles[n]:
 			shutil.copy(os.path.join(cpiodir, f), target)
 			os.chmod(os.path.join(target, f), stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR)
@@ -273,6 +409,15 @@ def main(argv):
 	else:
 		origin = options.origin
 
+	rpmdatabase = '/tmp/rpmdb.sqlite3'
+	#conn = sqlite3.connect(rpmdatabase)
+	#cursor = conn.cursor()
+	#cursor.execute('''create table if not exists rpm(rpmname text, checksum text, downloadurl text)''')
+	#cursor.execute('''create index if not exists rpm_checksum_index on rpm(checksum)''')
+	#cursor.execute('''create index if not exists rpm_rpmname_index on rpm(rpmname)''')
+	#cursor.close()
+	#conn.close()
+
 	if options.target == None:
 		target = tempfile.mkdtemp()[1]
 	else:
@@ -284,7 +429,7 @@ def main(argv):
 			#	pass
 			pass
 		target = options.target
-	unpacksrpm(options.filedir, target, unpacktmpdir)
+	unpacksrpm(options.filedir, target, unpacktmpdir, rpmdatabase)
 	generatelist(target, origin)
 
 if __name__ == "__main__":
