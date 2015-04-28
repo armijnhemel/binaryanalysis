b611932fdc86516033ffdd7f518d0833f99cc523
Armijn Hemel <armijn@tjaldur.nl>
2015-04-28 19:19:16 +0000

    more conversion

--- src/maintenance/bat-sqlitetopostgresql.py
+++ src/maintenance/bat-sqlitetopostgresql.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
+## Binary Analysis Tool
+## Copyright 2015 Armijn Hemel for Tjaldur Software Governance Solutions
+## Copyright 2015 Black Duck Software, Inc. All Rights Reserved.
+## Licensed under Apache 2.0, see LICENSE file for details
+
+'''
+Convert BAT databases from SQLite to PostgreSQL
+'''
+
 import os, sys, sqlite3
 import psycopg2
 from optparse import OptionParser
@@ -7,6 +16,8 @@ from optparse import OptionParser
 def main(argv):
 	parser = OptionParser()
 	parser.add_option("-d", "--database", action="store", dest="sqlitedb", help="path to SQLite database file", metavar="FILE")
+	parser.add_option("-l", "--licensedatabase", action="store", dest="licensesqlitedb", help="path to SQLite license database file", metavar="FILE")
+	parser.add_option("-f", "--filedatabase", action="store", dest="filesqlitedb", help="path to SQLite license database file", metavar="FILE")
 	(options, args) = parser.parse_args()
 	if options.sqlitedb == None:
 		parser.error("Specify SQLite database file")
@@ -87,14 +98,135 @@ def main(argv):
 		data = sqlitecursor.fetchmany(10000)
 	postgresqlconn.commit()
 
+	## then other stuff
+
+	## then all the kernel specific data
+	sqlitecursor.execute("select distinct * from kernel_configuration")
+	data = sqlitecursor.fetchmany(10000)
+	while data != []:
+		for d in data:
+			# kernel_configuration(configstring text, filename text, version text)
+			postgresqlcursor.execute("insert into kernel_configuration (configstring, filename, version) values (%s, %s, %s)", d)
+		postgresqlconn.commit()
+		data = sqlitecursor.fetchmany(10000)
+	postgresqlconn.commit()
+
+	sqlitecursor.execute("select distinct * from kernelmodule_alias")
+	data = sqlitecursor.fetchmany(10000)
+	while data != []:
+		for d in data:
+			# kernelmodule_alias(checksum text, modulename text, alias text)
+			postgresqlcursor.execute("insert into kernelmodule_alias (checksum, modulename, alias) values (%s, %s, %s)", d)
+		postgresqlconn.commit()
+		data = sqlitecursor.fetchmany(10000)
+	postgresqlconn.commit()
+
+	sqlitecursor.execute("select distinct * from kernelmodule_author")
+	data = sqlitecursor.fetchmany(10000)
+	while data != []:
+		for d in data:
+			# kernelmodule_alias(checksum text, modulename text, author text)
+			postgresqlcursor.execute("insert into kernelmodule_author (checksum, modulename, author) values (%s, %s, %s)", d)
+		postgresqlconn.commit()
+		data = sqlitecursor.fetchmany(10000)
+	postgresqlconn.commit()
+
+	sqlitecursor.execute("select distinct * from kernelmodule_description")
+	data = sqlitecursor.fetchmany(10000)
+	while data != []:
+		for d in data:
+			# kernelmodule_description(checksum text, modulename text, description text)
+			postgresqlcursor.execute("insert into kernelmodule_description (checksum, modulename, description) values (%s, %s, %s)", d)
+		postgresqlconn.commit()
+		data = sqlitecursor.fetchmany(10000)
+	postgresqlconn.commit()
+
+	sqlitecursor.execute("select distinct * from kernelmodule_firmware")
+	data = sqlitecursor.fetchmany(10000)
+	while data != []:
+		for d in data:
+			# kernelmodule_firmware(checksum text, modulename text, firmware text)
+			postgresqlcursor.execute("insert into kernelmodule_firmware (checksum, modulename, firmware) values (%s, %s, %s)", d)
+		postgresqlconn.commit()
+		data = sqlitecursor.fetchmany(10000)
+	postgresqlconn.commit()
+
+	sqlitecursor.execute("select distinct * from kernelmodule_license")
+	data = sqlitecursor.fetchmany(10000)
+	while data != []:
+		for d in data:
+			# kernelmodule_license(checksum text, modulename text, license text)
+			postgresqlcursor.execute("insert into kernelmodule_license (checksum, modulename, license) values (%s, %s, %s)", d)
+		postgresqlconn.commit()
+		data = sqlitecursor.fetchmany(10000)
+	postgresqlconn.commit()
+
+	sqlitecursor.execute("select distinct * from kernelmodule_parameter")
+	data = sqlitecursor.fetchmany(10000)
+	while data != []:
+		for d in data:
+			# kernelmodule_parameter(checksum text, modulename text, paramname text, paramtype text)
+			postgresqlcursor.execute("insert into kernelmodule_parameter(checksum, modulename, paramname, paramtype) values (%s, %s, %s, %s)", d)
+		postgresqlconn.commit()
+		data = sqlitecursor.fetchmany(10000)
+	postgresqlconn.commit()
+
+	sqlitecursor.execute("select distinct * from kernelmodule_parameter_description")
+	data = sqlitecursor.fetchmany(10000)
+	while data != []:
+		for d in data:
+			# kernelmodule_parameter_description(checksum text, modulename text, paramname text, description text)
+			postgresqlcursor.execute("insert into kernelmodule_parameter_description(checksum, modulename, paramname, description) values (%s, %s, %s, %s)", d)
+		postgresqlconn.commit()
+		data = sqlitecursor.fetchmany(10000)
+	postgresqlconn.commit()
+
+	sqlitecursor.execute("select distinct * from kernelmodule_version")
+	data = sqlitecursor.fetchmany(10000)
+	while data != []:
+		for d in data:
+			# kernelmodule_version(checksum text, modulename text, version text)
+			postgresqlcursor.execute("insert into kernelmodule_version(checksum, modulename, version) values (%s, %s, %s)", d)
+		postgresqlconn.commit()
+		data = sqlitecursor.fetchmany(10000)
+	postgresqlconn.commit()
+
+	## then all hashes -- hardcoded SHA256, SHA1, MD5, CRC32
+
+	sqlitecursor.close()
+	sqliteconn.close()
+
 	## then copy all the caches
+	## then any other database that might be kicking around
+	## first file lists
+	if options.filesqlitedb != None:
+		if not os.path.exists(options.filesqlitedb):
+			print >>sys.stderr, "SQLite file database file specified, but does not exist, exiting"
+			sys.exit(1)
+
+		## first set up sqlite cursor
+		sqliteconn = sqlite3.connect(options.filesqlitedb)
+		sqlitecursor = sqliteconn.cursor()
+		if cleandb:
+			postgresqlcursor.execute("delete from file")
+		sqlitecursor.execute("select distinct * from file")
+		data = sqlitecursor.fetchmany(10000)
+		while data != []:
+			for d in data:
+				# filename, directory, package, packageversion, source, distroversion
+				postgresqlcursor.execute("insert into file (filename, directory, package, packageversion, source, distroversion) values (%s, %s, %s, %s, %s, %s)", d)
+			postgresqlconn.commit()
+			data = sqlitecursor.fetchmany(10000)
+		postgresqlconn.commit()
+		sqlitecursor.close()
+		sqliteconn.close()
+
+	## then licenses and copyright
+	## then security
 
 	## finally clean up
 	postgresqlcursor.close()
 	postgresqlconn.close()
-
-	sqlitecursor.close()
-	sqliteconn.close()
 
 if __name__ == "__main__":
 	main(sys.argv)
