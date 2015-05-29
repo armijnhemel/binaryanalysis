3cede5c6e9ba52a411409fecd0107789ee5f66cf
Armijn Hemel <armijn@tjaldur.nl>
2015-05-29 16:52:10 +0000

    cleanup

--- src/extractkernelstrings.py
+++ src/extractkernelstrings.py
@@ -16,15 +16,5 @@ from bat import extractor
 ## macros being introduced (and removed) from the kernel sources regularly
 ## we should try and keep this list up to date.
 exprs = []
-exprs.append(re.compile("WIRELESS_SHOW\s*\((\w+),", re.MULTILINE))
-
-## some stuff that is used by __print_symbolic
-## TODO: add a proper expression for __print_symbolic
-## scsi_hostbyte_name, scsi_opcode_name, scsi_driverbyte_name
-## scsi_msgbyte_name, scsi_statusbyte_name
-exprs.append(re.compile("scsi_\w+_name\((\w+)", re.MULTILINE))
-
-## TODO: check if these can be replaced by a call to xgettext
-exprs.append(re.compile("E\((?:\w+,\s*)\"([\w\s\.:;<>\-+=~!@#$^%&*\[\]{}+?|/,'\(\)\\\]+)\"", re.MULTILINE))
 
 bugtrapexpr = re.compile("BUG_TRAP\s*\(([\w\s\.:<>\-+=~!@#$^%&*\[\]{}+?|/,'\(\)\\\]+)\);", re.MULTILINE)
