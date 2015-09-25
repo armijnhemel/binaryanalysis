2c64b95e3858d6c70b88e47ef0775a5fbe06f62b
Armijn Hemel <armijn@tjaldur.nl>
2015-09-25 11:45:13 +0000

    bump version number

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ import glob
 import os.path
 
 setup(name='bat',
-      version='22.0',
+      version='23.0',
       description='Binary Analysis Tool',
       author='Tjaldur Software Governance Solutions',
       author_email='info@binaryanalysis.org',
