e8a8460ff8748d8dfccddbc21ea33637c430d31f
Armijn Hemel <armijn@tjaldur.nl>
2016-04-25 08:57:20 +0000

    convenience queries to quickly drop all tables

---
+++ src/maintenance/postgresql-table-drop.sql
@@ -0,0 +1,66 @@
+drop table processed;
+drop table processed_file;
+drop table extracted_string;
+drop table extracted_function;
+drop table extracted_name;
+
+drop table kernel_configuration;
+drop table kernelmodule_alias;
+drop table kernelmodule_author;
+drop table kernelmodule_description;
+drop table kernelmodule_firmware;
+drop table kernelmodule_license;
+drop table kernelmodule_parameter;
+drop table kernelmodule_parameter_description;
+drop table kernelmodule_version;
+
+drop table rpm;
+drop table archivealias;
+drop table misc;
+drop table hashconversion;
+drop table licenses;
+drop table extracted_copyright;
+drop table security_cert;
+drop table security_cve;
+drop table security_password;
+drop table renames;
+drop table file;
+drop table stringscache_actionscript;
+drop table scores_actionscript;
+drop table avgstringscache_actionscript;
+
+drop table stringscache_c;
+drop table scores_c;
+drop table avgstringscache_c;
+
+drop table stringscache_csharp;
+drop table scores_csharp;
+drop table avgstringscache_csharp;
+
+drop table stringscache_java;
+drop table scores_java;
+drop table avgstringscache_java;
+
+drop table stringscache_javascript;
+drop table scores_javascript;
+drop table avgstringscache_javascript;
+
+drop table stringscache_php;
+drop table scores_php;
+drop table avgstringscache_php;
+
+drop table stringscache_python;
+drop table scores_python;
+drop table avgstringscache_python;
+
+drop table stringscache_ruby;
+drop table scores_ruby;
+drop table avgstringscache_ruby;
+
+drop table varnamecache_c;
+drop table linuxkernelnamecache;
+drop table functionnamecache_c;
+drop table linuxkernelfunctionnamecache;
+drop table functionnamecache_java;
+drop table fieldcache_java;
+drop table classcache_java;
