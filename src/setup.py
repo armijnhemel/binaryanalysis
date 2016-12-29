8ba9d606c363d71407913525a69ccab129469d6b
Armijn Hemel <armijn@tjaldur.nl>
2016-12-29 20:44:27 +0100

    preparing for 36

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ import glob
 import os.path
 
 setup(name='bat',
-      version='35.0',
+      version='36.0',
       description='Binary Analysis Tool',
       author='Binary Analysis Project',
       author_email='info@binaryanalysis.org',
