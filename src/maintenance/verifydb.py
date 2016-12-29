8ff63f76b5284273f70353f6a9b8b3fb8a8af994
Armijn Hemel <armijn@tjaldur.nl>
2016-12-29 19:46:16 +0100

    doc fixes for verifydb.py

--- src/maintenance/verifydb.py
+++ src/maintenance/verifydb.py
@@ -11,11 +11,7 @@ from optparse import OptionParser
 ## Licensed under Apache 2.0, see LICENSE file for details
 
 '''
-This script verifies that the tables in a database are in sync, which means: all of the files in the tables "extracted_string", "licenses" and "extracted_function" can also be found in "processed_file"
-
-Usage:
-
-python verifydb.py --database=/path/to/database
+This script verifies that the tables in a database are in sync, which means: all of the files in the tables "extracted_string" and "extracted_function" can also be found in "processed_file"
 '''
 
 def main(argv):
