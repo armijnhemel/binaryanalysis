10df0f1a2f25d031662020f0807e8652d4aa9977
Armijn Hemel <armijn@tjaldur.nl>
2021-01-21 20:22:45 +0100

    pylint fixes

--- src/bat/generatehexdump.py
+++ src/bat/generatehexdump.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-## Binary Analysis Tool
-## Copyright 2012-2016 Armijn Hemel for Tjaldur Software Governance Solutions
-## Licensed under Apache 2.0, see LICENSE file for details
+# Binary Analysis Tool
+# Copyright 2012-2016 Armijn Hemel for Tjaldur Software Governance Solutions
+# Licensed under Apache 2.0, see LICENSE file for details
 
 '''
 This is a plugin for the Binary Analysis Tool. It takes the output of hexdump -Cv
@@ -17,31 +17,36 @@ BAT_IMAGE_MAXFILESIZE :: maximum size of source file
 This should be run as a postrun scan
 '''
 
-import os, os.path, sys, subprocess, gzip
+import os
+import os.path
+import sys
+import subprocess
+import gzip
 
 def generateHexdump(filename, unpackreport, scantempdir, topleveldir, scanenv, cursor, conn, debug=False):
-	if not 'checksum' in unpackreport:
-		return
-	reportdir = scanenv.get('BAT_REPORTDIR', '.')
-	try:
-		os.stat(reportdir)
-	except:
-		## BAT_REPORTDIR does not exist
-		try:
-			os.makedirs(reportdir)
-		except Exception, e:
-			return
+    if not 'checksum' in unpackreport:
+        return
+    reportdir = scanenv.get('BAT_REPORTDIR', '.')
+    try:
+        os.stat(reportdir)
+    except:
+        # BAT_REPORTDIR does not exist
+        try:
+            os.makedirs(reportdir)
+        except Exception:
+            return
 
-	maxsize = int(scanenv.get('BAT_IMAGE_MAXFILESIZE', sys.maxint))
-	## override file name, we won't use it much
-	filename = os.path.join(unpackreport['realpath'], unpackreport['name'])
-	filesize = os.stat(filename).st_size
-	if filesize > maxsize:
-		return
-	if not os.path.exists("%s/%s-hexdump.gz" % (reportdir, unpackreport['checksum'])):
-		p = subprocess.Popen(['hexdump', '-Cv', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-		(stanout, stanerr) = p.communicate()
-		if stanout != "":
-			gf = gzip.open("%s/%s-hexdump.gz" % (reportdir, unpackreport['checksum']), 'w')
-			gf.write(stanout)
-			gf.close()
+    maxsize = int(scanenv.get('BAT_IMAGE_MAXFILESIZE', sys.maxint))
+    # override file name, we won't use it much
+    filename = os.path.join(unpackreport['realpath'], unpackreport['name'])
+    filesize = os.stat(filename).st_size
+    if filesize > maxsize:
+        return
+    if not os.path.exists("%s/%s-hexdump.gz" % (reportdir, unpackreport['checksum'])):
+        p = subprocess.Popen(['hexdump', '-Cv', filename], stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, close_fds=True)
+        (stanout, stanerr) = p.communicate()
+        if stanout != "":
+            gf = gzip.open("%s/%s-hexdump.gz" % (reportdir, unpackreport['checksum']), 'w')
+            gf.write(stanout)
+            gf.close()
--- src/bat/piecharts.py
+++ src/bat/piecharts.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-## Binary Analysis Tool
-## Copyright 2012-2015 Armijn Hemel for Tjaldur Software Governance Solutions
-## Licensed under Apache 2.0, see LICENSE file for details
+# Binary Analysis Tool
+# Copyright 2012-2015 Armijn Hemel for Tjaldur Software Governance Solutions
+# Licensed under Apache 2.0, see LICENSE file for details
 
 '''
 This is a plugin for the Binary Analysis Tool. It generates images of results
@@ -11,22 +11,24 @@ of the ranking scan, like piecharts and version charts.
 It is used by generateimages.py
 '''
 
-import os, os.path, sys, cPickle
+import os
+import os.path
+import cPickle
 import matplotlib
 matplotlib.use('cairo')
 import pylab
 
 def generateImages(picklefile, pickledir, filehash, imagedir, pietype):
 
-	leaf_file = open(os.path.join(pickledir, picklefile), 'rb')
-	(piedata, pielabels) = cPickle.load(leaf_file)
-	leaf_file.close()
+    leaf_file = open(os.path.join(pickledir, picklefile), 'rb')
+    (piedata, pielabels) = cPickle.load(leaf_file)
+    leaf_file.close()
 
-	pylab.figure(1, figsize=(6.5,6.5))
-	ax = pylab.axes([0.2, 0.15, 0.6, 0.6])
+    pylab.figure(1, figsize=(6.5,6.5))
+    ax = pylab.axes([0.2, 0.15, 0.6, 0.6])
 
-	pylab.pie(piedata, labels=pielabels)
+    pylab.pie(piedata, labels=pielabels)
 
-	pylab.savefig(os.path.join(imagedir, '%s-%s.png' % (filehash, pietype)))
-	pylab.gcf().clear()
-	os.unlink(os.path.join(pickledir, picklefile))
+    pylab.savefig(os.path.join(imagedir, '%s-%s.png' % (filehash, pietype)))
+    pylab.gcf().clear()
+    os.unlink(os.path.join(pickledir, picklefile))
